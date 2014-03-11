5060c8b5e7e5d7050918bf277b601f147db0af87
Sascha Dube <sascha.dube@gmail.com>
2014-03-11 17:13:53 +0100

    updated marketplace modal markup + css

--- extensions/system/assets/tmpl/marketplace.details.razr.php
+++ extensions/system/assets/tmpl/marketplace.details.razr.php
@@ -1,18 +1,18 @@
-<div class="uk-modal-dialog uk-modal-dialog-large">
+<div class="uk-modal-dialog uk-modal-dialog-large pk-marketplace-modal-dialog">
 
-    <div>
-        <h2>{{ title }}</h2>
+    <div class="pk-marketplace-modal-action">
         {{ #installed }}
         <button class="uk-button" disabled>@trans('Installed')</button>
         {{ /installed }}
+
         {{ ^installed }}
-            <button class="uk-button uk-button-primary" data-install="{{ name }}">
-                {{ #install }}@trans('Install'){{ /install }}
-                {{ #update }}@trans('Update'){{ /update }}
-            </button>
+        <button class="uk-button uk-button-primary" data-install="{{ name }}">
+            {{ #install }}@trans('Install'){{ /install }}
+            {{ #update }}@trans('Update'){{ /update }}
+        </button>
         {{ /installed }}
     </div>
 
-    <iframe src="{{ iframe }}" width="550" height="400" ></iframe>
+    <iframe src="{{ iframe }}" class="uk-width-1-1 uk-height-1-1"></iframe>
 
 </div>
