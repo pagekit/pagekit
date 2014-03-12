c59c7682c4d14f96eade96a6a503af6c029ac795
janschoenherr <jan@yootheme.com>
2014-03-12 11:13:38 +0100

    cleanup

--- extensions/system/assets/tmpl/feed.list.razr.php
+++ extensions/system/assets/tmpl/feed.list.razr.php
@@ -1,9 +1,9 @@
 <ul class="uk-list uk-list-line">
     {{ #items }}
     <li>
-        <a href="{{link}}">{{title}}</a> <span class="uk-text-muted uk-text-nowrap">{{publishedDate}}</span>
+        <a href="{{ link }}">{{ title }}</a> <span class="uk-text-muted uk-text-nowrap">{{ publishedDate }}</span>
         {{ #content }}
-        <p class="uk-margin-small-top">{{content}}</p>
+        <p class="uk-margin-small-top">{{ content }}</p>
         {{ /content }}
     </li>
     {{ /items }}
