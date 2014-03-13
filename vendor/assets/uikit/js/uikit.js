16b860af9c511ef7fe3b7cc9aa50372c89c785a7
Sascha Dube <sascha.dube@gmail.com>
2014-03-13 09:27:34 +0100

    updated vendor uikit js

--- vendor/assets/uikit/js/uikit.js
+++ vendor/assets/uikit/js/uikit.js
@@ -1475,7 +1475,7 @@
         if($element.data("nav")) return;
 
         this.options = $.extend({}, Nav.defaults, options);
-        this.element = $element.on("click", this.options.toggler, function(e) {
+        this.element = $element.on("click", this.options.toggle, function(e) {
             e.preventDefault();
 
             var ele = $(this);
@@ -1530,7 +1530,7 @@
     });
 
     Nav.defaults = {
-        "toggler": ">li.uk-parent > a[href='#']",
+        "toggle": ">li.uk-parent > a[href='#']",
         "lists": ">li.uk-parent > ul",
         "multiple": false
     };
@@ -1776,7 +1776,7 @@
 
         this.options = $.extend({}, Switcher.defaults, options);
 
-        this.element = $element.on("click", this.options.toggler, function(e) {
+        this.element = $element.on("click", this.options.toggle, function(e) {
             e.preventDefault();
             $this.show(this);
         });
@@ -1785,14 +1785,14 @@
 
             this.connect = $(this.options.connect).find(".uk-active").removeClass(".uk-active").end();
 
-            var togglers = this.element.find(this.options.toggler),
-                active   = togglers.filter(".uk-active");
+            var toggles = this.element.find(this.options.toggle),
+                active   = toggles.filter(".uk-active");
 
             if (active.length) {
                 this.show(active);
             } else {
-                active = togglers.eq(this.options.active);
-                this.show(active.length ? active : togglers.eq(0));
+                active = toggles.eq(this.options.active);
+                this.show(active.length ? active : toggles.eq(0));
             }
         }
 
@@ -1803,18 +1803,18 @@
 
         show: function(tab) {
 
-            tab = isNaN(tab) ? $(tab) : this.element.find(this.options.toggler).eq(tab);
+            tab = isNaN(tab) ? $(tab) : this.element.find(this.options.toggle).eq(tab);
 
             var active = tab;
 
             if (active.hasClass("uk-disabled")) return;
 
-            this.element.find(this.options.toggler).filter(".uk-active").removeClass("uk-active");
+            this.element.find(this.options.toggle).filter(".uk-active").removeClass("uk-active");
             active.addClass("uk-active");
 
             if (this.options.connect && this.connect.length) {
 
-                var index = this.element.find(this.options.toggler).index(active);
+                var index = this.element.find(this.options.toggle).index(active);
 
                 this.connect.children().removeClass("uk-active").eq(index).addClass("uk-active");
             }
@@ -1826,7 +1826,7 @@
 
     Switcher.defaults = {
         connect : false,
-        toggler : ">*",
+        toggle : ">*",
         active  : 0
     };
 
@@ -1890,7 +1890,7 @@
             if (!$(this).parents(".uk-disabled:first").length) ul.append(item);
         });
 
-        this.element.uk("switcher", {"toggler": ">li:not(.uk-tab-responsive)", "connect": this.options.connect, "active": this.options.active});
+        this.element.uk("switcher", {"toggle": ">li:not(.uk-tab-responsive)", "connect": this.options.connect, "active": this.options.active});
 
         mobiletab.append(dropdown).uk("dropdown", {"mode": "click"});
 
