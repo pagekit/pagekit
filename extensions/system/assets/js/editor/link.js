e840f06ddb068e52e51a0ccfacc3cb4f72615cfa
janschoenherr <jan@yootheme.com>
2014-03-20 13:30:37 +0100

    updated editor.link.js

--- extensions/system/assets/js/editor/link.js
+++ extensions/system/assets/js/editor/link.js
@@ -2,9 +2,8 @@ define(['jquery', 'tmpl!link.modal,link.replace', 'uikit', 'link'], function($, 
 
     var modal  = $(tmpl.get('link.modal')).appendTo('body'),
         picker = new uikit.modal.Modal(modal),
-        url    = modal.find('.js-link-url'),
         title  = modal.find('.js-title'),
-        link   = new Link(modal),
+        link   = new Link(modal.find('.js-linkpicker')),
         handler;
 
     modal.on('click', '.js-update', function() {
@@ -15,7 +14,7 @@ define(['jquery', 'tmpl!link.modal,link.replace', 'uikit', 'link'], function($, 
 
         markdownarea.addPlugin('urls', /(?:\[([^\n\]]*)\])(?:\(([^\n\]]*)\))?/gim, function(marker) {
 
-            if(marker.found[4] && marker.found[4].indexOf("!"+marker.found[0])!=-1) {
+            if (marker.found[4] && marker.found[4].indexOf("!"+marker.found[0])!=-1) {
                 return marker.found[0];
             }
 
@@ -25,14 +24,13 @@ define(['jquery', 'tmpl!link.modal,link.replace', 'uikit', 'link'], function($, 
 
                 handler = function() {
                     picker.hide();
-                    marker.replace('[' + title.val() + '](' + url.val() + ')');
+                    marker.replace('[' + title.val() + '](' + link.getValue() + ')');
                 };
 
-                url.val(marker.found[2]);
                 title.val(marker.found[1]);
                 picker.show();
                 setTimeout(function() { title.focus(); }, 10);
-                link.init();
+                link.init(marker.found[2]);
             });
 
             return tmpl.render('link.replace', { marker: marker, link: marker.found[2].trim() ? marker.found[2] : null, txt:  marker.found[1].trim() ? marker.found[1] : null }).replace(/(\r\n|\n|\r)/gm, '');
