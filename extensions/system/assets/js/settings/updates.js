20454e1e242992cc742f8a438015e19cad45aa7c
Steffan Schiewe <steffan@schiewe.biz>
2014-03-25 17:52:47 +0100

    Removed form-file requirement

--- extensions/system/assets/js/settings/updates.js
+++ extensions/system/assets/js/settings/updates.js
@@ -1,4 +1,4 @@
-require(['jquery', 'marketplace', 'tmpl!package.updates,package.upload', 'uikit!form-file', 'locale', 'domReady!'], function($, marketplace, tmpl, uikit, locale) {
+require(['jquery', 'marketplace', 'tmpl!package.updates,package.upload', 'uikit', 'locale', 'domReady!'], function($, marketplace, tmpl, uikit, locale) {
 
     var page = $('#js-extensions, #js-themes'), update = $('.js-update', page), upload = $('.js-upload', page), market = $('.js-marketplace', page), params = page.data(), packages = {}, modal;
 
