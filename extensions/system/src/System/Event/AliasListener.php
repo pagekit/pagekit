84b4218b56b5720bdb106cb4fd44e83f4f410ab3
janschoenherr <jan@yootheme.com>
2014-03-26 13:49:47 +0100

    fixed aliases

--- extensions/system/src/System/Event/AliasListener.php
+++ extensions/system/src/System/Event/AliasListener.php
@@ -21,7 +21,7 @@ class AliasListener extends EventSubscriber
         }
 
         foreach ($aliases as $alias) {
-            $manager->register($alias->getAlias(), $this('url')->route($alias->getSource()));
+            $manager->register($alias->getAlias(), $this('url')->route($alias->getSource(), array(), 'base'));
         }
     }
 
