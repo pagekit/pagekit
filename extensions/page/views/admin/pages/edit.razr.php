6b09c9e40e20981cc684656416cd05cf6d62164d
Sascha Dube <sascha.dube@gmail.com>
2014-03-28 14:18:37 +0100

    first draft for changing pages settings

--- extensions/page/views/admin/pages/edit.razr.php
+++ extensions/page/views/admin/pages/edit.razr.php
@@ -11,6 +11,28 @@
             <div class="uk-form-row">
                 <input class="uk-width-1-1 uk-form-large" type="text" name="page[title]" value="@page.Title" placeholder="@trans('Enter Title')" required>
             </div>
+
+            <div class="uk-form-row">
+                @trans('Slug'):
+                <a class="js-slug" href="#" data-uk-toggle="{target:'.js-slug'}">@page.slug</a>
+                <input class="uk-form-width-medium uk-form-small uk-hidden js-slug" type="text" name="page[slug]" value="@page.slug">
+            </div>
+
+            <div class="uk-form-row">
+                <ul class="uk-subnav uk-">
+                    <li>@trans('Status'): @( page.status ? '<span class="uk-text-success">Published</span>' : '<span class="uk-text-danger">Unpublished</span>' )</li>
+                    <li>
+                        @trans('Access'):
+                        <a class="js-access" href="#" data-uk-toggle="{target:'.js-access'}">Public</a>
+                        <select class="uk-form-width-small uk-form-small uk-hidden js-access" name="page[access_id]">
+                            @foreach (levels as level)
+                            <option value="@level.id"@(page.accessId == level.id ? ' selected' : '')>@level.name</option>
+                            @endforeach
+                        </select>
+                    </li>
+                </ul>
+            </div>
+
             <div class="uk-form-row">
                 @editor('page[content]', page.content, ['id' => 'page-content'])
             </div>
