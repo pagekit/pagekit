4d7cae0d1d1137918bd8775a0438fa518774e51b
Artur Heinze <artur@yootheme.com>
2014-04-07 16:27:21 +0200

    cleanup

--- extensions/system/assets/js/editor/markdown.js
+++ extensions/system/assets/js/editor/markdown.js
@@ -9,15 +9,8 @@ require(['jquery', 'uikit!htmleditor', 'marked', 'codemirror', 'domReady!'], fun
             arguments[plugin](uikit.htmleditor, options);
         }
 
-        uikit.htmleditor.defaults.codemirror.autoCloseTags = true;
-        uikit.htmleditor.defaults.codemirror.matchTags     = true;
-
         $('[data-editor="markdown"]').each(function() {
             var editor = new uikit.htmleditor(this, { marked: marked, CodeMirror: codemirror, markdown:true });
-
-            editor.editor.on('inputRead', uikit.Utils.debounce(function() {
-                autocomplete(editor.editor);
-            }, 100));
 
             editor.editor.addKeyMap({
                 'Ctrl-S': function() { save(editor.element[0]); },
@@ -35,18 +28,4 @@ require(['jquery', 'uikit!htmleditor', 'marked', 'codemirror', 'domReady!'], fun
             textarea.form.submit();
         }
     }
-
-
-    function autocomplete(cm) {
-        var doc = cm.getDoc(), POS = doc.getCursor(), mode = CodeMirror.innerMode(cm.getMode(), cm.getTokenAt(POS).state).mode.name;
-
-        if (mode == 'xml') { //html depends on xml
-
-            var cur = cm.getCursor(), token = cm.getTokenAt(cur);
-
-            if (token.string.charAt(0) == "<" || token.type == "attribute") {
-                CodeMirror.showHint(cm, CodeMirror.hint.html, { completeSingle: false });
-            }
-        }
-    };
 });
