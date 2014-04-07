42aadfd8eb5544d9dc26509df37f6902a4c8f8c8
Florian Letsch <florianletsch@gmail.com>
2014-04-07 16:21:24 +0200

    Translation demo

--- extensions/hello/views/greet.razr.php
+++ extensions/hello/views/greet.razr.php
@@ -1,7 +1,9 @@
 @style('hello-site', 'hello/css/hello.css')
 
+<p>@transchoice("{0}: No names|one: One name|more: %names% names", names|length, ["%names%" => names|length])<p>
+
 @foreach(names as name)
-<p>
-    Hello @name!
-</p>
+    <p>
+        @trans("Hello %name%!", ["%name%" => name])
+    </p>
 @endforeach
