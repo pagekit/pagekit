b7a0c8d6f92b65238a8b5d95fb33e85481a61168
Artur Heinze <artur@yootheme.com>
2014-04-10 13:44:23 +0200

    pagination add-on updated

--- vendor/assets/uikit/js/addons/pagination.js
+++ vendor/assets/uikit/js/addons/pagination.js
@@ -1,3 +1,8 @@
+/*! UIkit 2.6.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
+
+/*
+ * Based on simplePagination - Copyright (c) 2012 Flavius Matis - http://flaviusmatis.github.com/simplePagination.js/ (MIT)
+ */
 (function(addon) {
 
     if (typeof define == "function" && define.amd) { // AMD
@@ -28,99 +33,81 @@
 
         $element.data("pagination", this);
 
-        this._draw();
+        this._render();
     };
 
 
     $.extend(Pagination.prototype, {
 
-
         _getInterval: function() {
 
             return {
                 start: Math.ceil(this.currentPage > this.halfDisplayed ? Math.max(Math.min(this.currentPage - this.halfDisplayed, (this.pages - this.options.displayedPages)), 0) : 0),
-                end: Math.ceil(this.currentPage > this.halfDisplayed ? Math.min(this.currentPage + this.halfDisplayed, this.pages) : Math.min(this.options.displayedPages, this.pages))
+                end  : Math.ceil(this.currentPage > this.halfDisplayed ? Math.min(this.currentPage + this.halfDisplayed, this.pages) : Math.min(this.options.displayedPages, this.pages))
             };
         },
 
-        redraw: function(pages) {
-
-            if(pages) {
-                this.pages = pages;
-            }
-
-            this._draw();
+        render: function(pages) {
+            this.pages = pages ? pages : this.pages;
+            this._render();
         },
 
         selectPage: function(pageIndex) {
             this.currentPage = pageIndex;
-            this._draw();
+            this._render();
         },
 
-        _draw: function() {
+        _render: function() {
 
             var o = this.options, interval = this._getInterval(), i;
 
             this.element.empty();
 
             // Generate Prev link
-            if (o.prevText) {
-                this._appendItem(o.currentPage - 1, {text: o.prevText});
-            }
-
-            // Generate Next link (if option set for at front)
-            if (o.nextText && o.nextAtFront) {
-                this._appendItem(o.currentPage + 1, {text: o.nextText});
-            }
+            if (o.lblPrev) this._append(o.currentPage - 1, {text: o.lblPrev});
 
             // Generate start edges
             if (interval.start > 0 && o.edges > 0) {
+
                 var end = Math.min(o.edges, interval.start);
-                for (i = 0; i < end; i++) {
-                    this._appendItem(i);
-                }
+
+                for (i = 0; i < end; i++) this._append(i);
+
                 if (o.edges < interval.start && (interval.start - o.edges != 1)) {
                     this.element.append('<li><span>...</span></li>');
                 } else if (interval.start - o.edges == 1) {
-                    this._appendItem(o.edges);
+                    this._append(o.edges);
                 }
             }
 
             // Generate interval links
-            for (i = interval.start; i < interval.end; i++) {
-                this._appendItem(i);
-            }
+            for (i = interval.start; i < interval.end; i++) this._append(i);
 
             // Generate end edges
-            if (interval.end < o.pages && o.edges > 0) {
-                if (o.pages - o.edges > interval.end && (o.pages - o.edges - interval.end != 1)) {
+            if (interval.end < this.pages && o.edges > 0) {
+
+                if (this.pages - o.edges > interval.end && (this.pages - o.edges - interval.end != 1)) {
                     this.element.append('<li><span>...</span></li>');
-                } else if (o.pages - o.edges - interval.end == 1) {
-                    this._appendItem(interval.end++);
+                } else if (this.pages - o.edges - interval.end == 1) {
+                    this._append(interval.end++);
                 }
-                var begin = Math.max(o.pages - o.edges, interval.end);
-                for (i = begin; i < o.pages; i++) {
-                    this._appendItem(i);
-                }
+
+                var begin = Math.max(this.pages - o.edges, interval.end);
+
+                for (i = begin; i < this.pages; i++) this._append(i);
             }
 
             // Generate Next link (unless option is set for at front)
-            if (o.nextText && !o.nextAtFront) {
-                this._appendItem(o.currentPage + 1, {text: o.nextText, classes: 'next'});
-            }
+            if (o.lblNext) this._append(o.currentPage + 1, {text: o.lblNext});
+
         },
 
-        _appendItem: function(pageIndex, opts) {
+        _append: function(pageIndex, opts) {
 
             var $this = this, item, link, options;
 
             pageIndex = pageIndex < 0 ? 0 : (pageIndex < this.pages ? pageIndex : this.pages - 1);
-
-            var options = {
-                text: pageIndex + 1
-            };
-
-            options = $.extend(options, opts || {});
+            options   = $.extend({ text: pageIndex + 1 }, opts);
 
             if (pageIndex == this.currentPage) {
                 item = '<li class="uk-active"><span>' + (options.text) + '</span></li>';
@@ -141,12 +128,14 @@
     });
 
     Pagination.defaults = {
-        items          : 100,
+        items          : 1,
         itemsOnPage    : 1,
         pages          : 0,
-        displayedPages : 10,
+        displayedPages : 3,
         edges          : 3,
         currentPage    : 1,
+        lblPrev        : false,
+        lblNext        : false,
         onSelectPage   : function() {}
     };
 
