02b85681598965f8289b3c34b15847f2856f1431
Steffan Schiewe <steffan@schiewe.biz>
2014-04-10 12:24:52 +0200

    Renamed vars

--- extensions/page/assets/js/edit.js
+++ extensions/page/assets/js/edit.js
@@ -1,63 +1,64 @@
 require(['jquery', 'uikit!datepicker,timepicker', 'domReady!'], function($, uikit) {
 
-    var $form = $('#js-page'), $id = $('input[name="id"]', $form);
+    var form = $('#js-page'), id = $('input[name="id"]', form);
 
     // slug handling
-    var $slug  = $('input[name="page[slug]"]', $form),
-        $title = $('input[name="page[title]"]', $form);
+    var slug  = $('input[name="page[slug]"]', form),
+        title = $('input[name="page[title]"]', form);
 
-    $title.on('blur', function () {
-        if (!($id.val()-0)) $slug.val('');
-        $slug.trigger('blur');
+    title.on('blur', function () {
+        if (!(id.val()-0)) slug.val('');
+        slug.trigger('blur');
     });
 
-    $slug.on('blur', function() {
-        $.post($slug.data('url'), { slug: $slug.val() ? $slug.val() : $title.val(), id: $id.val() }, function(data) {
-            $slug.val(data).addClass('uk-hidden');
+    slug.on('blur', function() {
+        $.post(slug.data('url'), { slug: slug.val() ? slug.val() : title.val(), id: id.val() }, function(data) {
+            slug.val(data).addClass('uk-hidden');
             $('.js-slug[data-uk-toggle]').text(data).removeClass('uk-hidden');
         }, 'json');
     });
 
     // status handling
-    var $status   = $('input[name="page[status]"]', $form),
-        $statuses = $('.js-status', $form).on('click', function(e){
+    var status   = $('input[name="page[status]"]', form),
+        statuses = $('.js-status', form).on('click', function(e){
             e.preventDefault();
-            $status.val($statuses.addClass('uk-hidden').not(this).removeClass('uk-hidden').data('status'));
+            status.val(statuses.addClass('uk-hidden').not(this).removeClass('uk-hidden').data('status'));
         });
 
-    if ($status.val() == '') $status.val(0);
+    if (status.val() === '') status.val(0);
 
-    $statuses.eq($status.val()).removeClass('uk-hidden');
+    statuses.eq(status.val()).removeClass('uk-hidden');
 
     // access handling
     $('a.js-access').text($('select.js-access option:selected').text());
 
     // markdown status handling
-    var $markdownStatus   = $('input[name="page[data][markdown]"]'),
-        $markdownStatuses = $('.js-markdown').on('click', function(e){
+    var markdownStatus   = $('input[name="page[data][markdown]"]'),
+        markdownStatuses = $('.js-markdown').on('click', function(e){
             e.preventDefault();
-            $markdownStatus.val($markdownStatuses.addClass('uk-hidden').not(this).removeClass('uk-hidden').data('value'));
-            $('#page-content', $form).trigger($markdownStatus.val() == '1' ? 'enableMarkdown' : 'disableMarkdown');
+            markdownStatus.val(markdownStatuses.addClass('uk-hidden').not(this).removeClass('uk-hidden').data('value'));
+            $('#page-content', form).trigger(markdownStatus.val() == '1' ? 'enableMarkdown' : 'disableMarkdown');
         });
 
     // date handling
-    $form.on('change', '.js-publish', function() {
-        $('[name="page[date]"]', $form).val($('[data-uk-datepicker]', $form).val()+' '+$('[data-uk-timepicker]', $form).val());
+    form.on('change', '.js-publish', function() {
+        $('[name="page[date]"]', form).val($('[data-uk-datepicker]', form).val()+' '+$('[data-uk-timepicker]', form).val());
     });
 
     // form ajax saving
-    $form.on('submit', function(e) {
+    form.on('submit', function(e) {
 
         e.preventDefault();
         e.stopImmediatePropagation();
 
-        $.post($form.attr('action'), $form.serialize(), function(response) {
+        $.post(form.attr('action'), form.serialize(), function(response) {
 
             uikit.notify(response.message, response.error ? 'danger' : 'success');
 
             if (response.id) {
-                $id.val(response.id);
+                id.val(response.id);
             }
         });
     });
+
 });
--- extensions/page/assets/js/pages.js
+++ extensions/page/assets/js/pages.js
@@ -1,20 +1,21 @@
 require(['jquery', 'domReady!'], function($) {
 
-    var $form = $('#js-pages');
+    var form = $('#js-pages');
 
     // action button
-    $form.on('click', '[data-action]', function(e) {
+    form.on('click', '[data-action]', function(e) {
         e.preventDefault();
-        $form.attr('action', $(this).data('action')).submit();
+        form.attr('action', $(this).data('action')).submit();
     });
 
     // submit filters
-    $form.on('change', 'select[name^="filter"]', function() {
-        $form.submit();
+    form.on('change', 'select[name^="filter"]', function() {
+        form.submit();
     });
 
     // select all checkbox
-    $form.on('click', '.js-select-all:checkbox', function() {
-        $('[name="ids[]"]:checkbox', $form).prop('checked', $(this).prop('checked'));
+    form.on('click', '.js-select-all:checkbox', function() {
+        $('[name="ids[]"]:checkbox', form).prop('checked', $(this).prop('checked'));
     });
+
 });
