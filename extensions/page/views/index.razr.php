6639c6985910f7419600457f015bd04204630d54
Artur Heinze <artur@yootheme.com>
2014-04-14 11:32:30 +0200

    show page title setting

--- extensions/page/views/index.razr.php
+++ extensions/page/views/index.razr.php
@@ -1,4 +1,7 @@
 <article class="uk-article">
+    @if(page.get('title', true))
     <h1 class="uk-article-title">@page.title</h1>
+    @endif
+
     @app.content.applyPlugins(page.content, ['markdown' => page.get('markdown'), 'page' => page])
 </article>
