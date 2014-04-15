f6b7af260eb643c8bf9881fe07813231c3d21c57
Sascha Dube <sascha.dube@gmail.com>
2014-04-15 17:58:13 +0200

    cleanup widget parameters in alpha theme

--- themes/alpha/views/admin/widgets/edit.razr.php
+++ themes/alpha/views/admin/widgets/edit.razr.php
@@ -1,45 +1,41 @@
 <div class="uk-form-row">
-    <label for="form-panel" class="uk-form-label">@trans('Panel Style')</label>
+    <label for="form-theme-panel" class="uk-form-label">@trans('Panel Style')</label>
     <div class="uk-form-controls">
-        <select id="form-panel" class="uk-form-width-large" name="theme_alpha[panel]">
+        <select id="form-theme-panel" class="uk-form-width-large" name="theme_alpha[panel]">
             @foreach ([
-                'uk-panel'                                     => trans('None'),
-                'uk-panel uk-panel-box'                        => trans('Box'),
-                'uk-panel uk-panel-box uk-panel-box-primary'   => trans('Box Primary'),
-                'uk-panel uk-panel-box uk-panel-box-secondary' => trans('Box Secondary'),
-                'uk-panel uk-panel-header'                     => trans('Header'),
-                'uk-panel uk-panel-space'                      => trans('Space')
+                ''                                    => trans('None'),
+                'uk-panel-box'                        => trans('Box'),
+                'uk-panel-box uk-panel-box-primary'   => trans('Box Primary'),
+                'uk-panel-box uk-panel-box-secondary' => trans('Box Secondary'),
+                'uk-panel-header'                     => trans('Header'),
+                'uk-panel-space'                      => trans('Space')
             ] as value => name)
-            <option value="@value"@((settings[widget.id]['panel'] ?: 'uk-panel uk-panel-box') == value ? ' selected')>@name</option>
+            <option value="@value"@((settings[widget.id]['panel'] ?: '') == value ? ' selected')>@name</option>
             @endforeach
         </select>
     </div>
 </div>
 
 <div class="uk-form-row">
-    <label for="form-title" class="uk-form-label">@trans('Badge')</label>
+    <label for="form-theme-badge" class="uk-form-label">@trans('Badge')</label>
     <div class="uk-form-controls">
-        <p class="uk-form-controls-condensed">
-            <select id="form-panel" class="uk-form-width-large" name="theme_alpha[badge][type]">
-                @foreach ([
-                    'uk-panel-badge uk-badge'                  => trans('None'),
-                    'uk-panel-badge uk-badge uk-badge-success' => trans('Success'),
-                    'uk-panel-badge uk-badge uk-badge-warning' => trans('Warning'),
-                    'uk-panel-badge uk-badge uk-badge-danger'  => trans('Danger')
-                ] as value => name)
-                <option value="@value"@((settings[widget.id]['badge']['type'] ?: 'uk-panel-badge uk-badge') == value ? ' selected')>@name</option>
-                @endforeach
-            </select>
-            <input class="uk-form-width-large" type="text" name="theme_alpha[badge][text]" value="@settings[widget.id]['badge']['text']" placeholder="@trans('Badge Text')">
-        </p>
+        <input id="form-theme-badge" class="uk-form-width-small" type="text" name="theme_alpha[badge][text]" value="@settings[widget.id]['badge']['text']">
+        <select class="uk-form-width-small" name="theme_alpha[badge][type]">
+            @foreach ([
+                'uk-panel-badge uk-badge'                  => trans('Default'),
+                'uk-panel-badge uk-badge uk-badge-success' => trans('Success'),
+                'uk-panel-badge uk-badge uk-badge-warning' => trans('Warning'),
+                'uk-panel-badge uk-badge uk-badge-danger'  => trans('Danger')
+            ] as value => name)
+            <option value="@value"@((settings[widget.id]['badge']['type'] ?: 'uk-panel-badge uk-badge') == value ? ' selected')>@name</option>
+            @endforeach
+        </select>
     </div>
 </div>
 
 <div class="uk-form-row">
     <span class="uk-form-label">@trans('Alignment')</span>
     <div class="uk-form-controls uk-form-controls-text">
-        <p class="uk-form-controls-condensed">
-            <label><input type="checkbox" name="theme_alpha[alignment]" value="center-content" @(settings[widget.id]['alignment'] == 'center-content' ? 'checked':'')> @trans('Center Content')</label>
-        </p>
+        <label><input type="checkbox" name="theme_alpha[alignment]" value="center-content" @(settings[widget.id]['alignment'] == 'center-content' ? 'checked':'')> @trans('Center the title and content.')</label>
     </div>
 </div>
