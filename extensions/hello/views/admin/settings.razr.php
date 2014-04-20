2c008bb44a903e1c5188b6ca63a8dddb0d63f33c
Sascha Dube <sascha.dube@gmail.com>
2014-04-20 09:35:11 +0200

    cleanup hello extension

--- extensions/hello/views/admin/settings.razr.php
+++ extensions/hello/views/admin/settings.razr.php
@@ -1,12 +1,12 @@
 <form class="uk-form uk-form-horizontal" action="@url.route('@system/extensions/savesettings', ['name' => 'hello'])" method="post">
 
-    <p>
-        @trans('This settings page is just for demonstration purposes.')
-    </p>
-
-    <p>
+	<div class="pk-options">
         <button class="uk-button uk-button-primary" type="submit">@trans('Save')</button>
         <a class="uk-button" href="@url.route('@system/system/index')">@trans('Close')</a>
+    </div>
+
+    <p>
+        @trans('This settings page is just for demonstration purposes.')
     </p>
 
     @token()
