38e63fbef9b134d0d62d49d12643e819ba3c9ac6
Florian Letsch <florianletsch@gmail.com>
2014-04-22 17:54:16 +0200

    hello extension: store dummy config

--- extensions/hello/views/admin/settings.razr.php
+++ extensions/hello/views/admin/settings.razr.php
@@ -1,5 +1,12 @@
 <form class="uk-form uk-form-horizontal" action="@url.route('@system/extensions/savesettings', ['name' => 'hello'])" method="post">
 
+    <div class="uk-form-row">
+        <label for="form-hello-message" class="uk-form-label">@trans('Message')</label>
+        <div class="uk-form-controls">
+            <input id="form-hello-message" type="text" name="config[message]" value="@config['message']">
+        </div>
+    </div>
+
 	<div class="pk-options">
         <button class="uk-button uk-button-primary" type="submit">@trans('Save')</button>
         <a class="uk-button" href="@url.route('@system/system/index')">@trans('Close')</a>
