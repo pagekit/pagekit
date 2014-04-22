c45e5c10da944573f6ed2017b6ae34f33eda37cd
Artur Heinze <artur@yootheme.com>
2014-04-22 15:15:00 +0200

    image + video editor add-ons reworked

--- extensions/system/assets/js/editor/image.js
+++ extensions/system/assets/js/editor/image.js
@@ -1,98 +1,115 @@
 define(['jquery', 'tmpl!image.modal,image.replace', 'uikit', 'finder'], function($, tmpl, uikit, Finder) {
 
-    var base      = requirejs.toUrl(''),
-        modal     = $(tmpl.render('image.modal')).appendTo('body'),
-        element   = modal.find('.js-finder'),
-        image     = modal.find('.js-url'),
-        title     = modal.find('.js-title'),
-        preview   = modal.find('.js-img-preview'),
-        btnselect = modal.find('.js-select-image'),
-        screens   = modal.find('[data-screen]').css({'animation-duration':'0.1s', '-webkit-animation-duration':'0.1s'}),
-        goto      = function(screen) {
+    var ImagePopup = {
 
-            var next = screens.filter('[data-screen="'+screen+'"]');
+        init: function(options){
 
-            screens.addClass('uk-hidden')
-            next.removeClass('uk-hidden');
+            var $this = this;
 
-            picker.updateScrollable();
+            this.options   = options;
+
+            this.base      = requirejs.toUrl('');
+            this.modal     = $(tmpl.render('image.modal')).appendTo('body');
+            this.element   = this.modal.find('.js-finder');
+            this.image     = this.modal.find('.js-url');
+            this.title     = this.modal.find('.js-title');
+            this.preview   = this.modal.find('.js-img-preview');
+            this.btnselect = this.modal.find('.js-select-image');
+            this.screens   = this.modal.find('[data-screen]').css({'animation-duration':'0.1s', '-webkit-animation-duration':'0.1s'});
+            this.finder    = null;
+            this.picker    = null;
+            this.handler   = null;
+
+            // events
+            this.modal.on('click', '.js-update', function() {
+                $this.handler();
+            });
+
+            this.modal.on('click', '[data-goto]', function(e){
+                e.preventDefault();
+                $this.goto($(this).data('goto'));
+            });
+
+            this.element.on('selected-rows', function(e, rows) {
+
+                if (rows.length === 1) {
+
+                    var data = $(rows[0]).data();
+
+                    if (data.type == 'file' && data.url.match(/\.(png|jpg|jpeg|gif|svg)$/i)) {
+                        $this.btnselect.prop('disabled', false).data('url', data.url);
+                    }
+
+                } else {
+                    $this.btnselect.prop('disabled', true);
+                }
+            });
+
+            this.btnselect.on('click', function() {
+
+                var url = $this.btnselect.data('url');
+
+                $this.updatePreview(url);
+
+                // convert to relative urls
+                if (url.indexOf($this.base) === 0) {
+                    url = url.replace($this.base, '');
+                }
+
+                $this.image.val(url);
+                $this.goto('settings');
+            });
         },
 
-        finder, handler, picker;
+        getPicker: function() {
 
-    modal.on('click', '.js-update', function() {
-        handler();
-    });
+            if(!this.picker) {
 
-    modal.on('click', '[data-goto]', function(e){
-        e.preventDefault();
-        goto($(this).data('goto'));
-    });
-
-    element.on('selected-rows', function(e, rows) {
-
-        if (rows.length === 1) {
-
-            var data = $(rows[0]).data();
-
-            if (data.type == 'file' && data.url.match(/\.(png|jpg|jpeg|gif|svg)$/i)) {
-                btnselect.prop('disabled', false).data('url', data.url);
+                this.finder = new Finder(this.element, this.options);
+                this.element.find('.js-finder-files').addClass('uk-overflow-container');
+                this.picker = new uikit.modal.Modal(this.modal);
+                this.element.find('.js-finder-toolbar-left').prepend(this.btnselect);
             }
 
-        } else {
-            btnselect.prop('disabled', true);
+            return this.picker;
+        },
+
+        goto: function(screen){
+            var next = this.screens.filter('[data-screen="'+screen+'"]');
+
+            this.screens.addClass('uk-hidden');
+            next.removeClass('uk-hidden');
+
+            this.getPicker().updateScrollable();
+        },
+
+        updatePreview: function(url) {
+
+            var $this = this;
+
+            // convert to relative urls
+            if (url && !url.match(/^(\/|http\:|https\:|ftp\:)/i)) {
+                url = this.base + '/' + url;
+            }
+
+            var pimg = new Image();
+
+            pimg.onerror = function(){
+                $this.preview.attr('src', $this.base+'extensions/system/assets/images/placeholder-editor-image.svg');
+            };
+
+            pimg.onload = function(){
+                $this.preview.attr('src', url);
+            };
+
+            pimg.src = url;
         }
-    });
 
-    btnselect.on('click', function() {
-
-        var url = btnselect.data('url');
-
-        updatePreview(url);
-
-        // convert to relative urls
-        if (url.indexOf(base) === 0) {
-            url = url.replace(base, '');
-        }
-
-        image.val(url);
-
-        goto('settings');
-    });
-
-
-    // preview
-    var pimg;
-
-    function updatePreview(url) {
-
-        // convert to relative urls
-        if (url && !url.match(/^(\/|http\:|https\:|ftp\:)/i)) {
-            url = base + '/' + url;
-        }
-
-        var pimg = new Image();
-
-        pimg.onerror = function(){
-            preview.attr('src', base+'extensions/system/assets/images/placeholder-editor-image.svg');
-        };
-
-        pimg.onload = function(){
-            preview.attr('src', url);
-        };
-
-        pimg.src = url;
-    }
-
-    function initFinder(options) {
-        finder = new Finder(element, options);
-        element.find('.js-finder-files').addClass('uk-overflow-container');
-        picker = new uikit.modal.Modal(modal);
-
-        element.find('.js-finder-toolbar-left').prepend(btnselect);
-    }
+    };
 
     return function(htmleditor, options, editors) {
+
+        ImagePopup.init(options);
 
         editors = editors || [];
 
@@ -111,27 +128,24 @@ define(['jquery', 'tmpl!image.modal,image.replace', 'uikit', 'finder'], function
             attrs.src = img.attr('src') || '';
             attrs.alt = img.attr('alt') || '';
 
-            if (!finder) {
-                initFinder(options);
-            }
-
             marker.editor.preview.on('click', '#' + marker.uid + ' .js-config', function() {
-                title.val(attrs.alt);
-                image.val(attrs.src);
+
+                ImagePopup.title.val(attrs.alt);
+                ImagePopup.image.val(attrs.src);
+
+                ImagePopup.updatePreview(ImagePopup.image.val());
+                ImagePopup.goto('settings');
+                ImagePopup.getPicker().show();
 
                 //load finder in image dir
-                finder.loadPath(attrs.src.trim() && attrs.src.indexOf(rootpath) === 0 ? attrs.src.replace(rootpath, '').split('/').slice(0, -1).join('/') : '');
+                ImagePopup.finder.loadPath(attrs.src.trim() && attrs.src.indexOf(rootpath) === 0 ? attrs.src.replace(rootpath, '').split('/').slice(0, -1).join('/') : '');
 
-                updatePreview(image.val());
-                goto('settings');
-                picker.show();
+                setTimeout(function() { ImagePopup.title.focus(); }, 10);
 
-                setTimeout(function() { title.focus(); }, 10);
-
-                handler = function() {
-                    picker.hide();
-                    img.attr('src', image.val());
-                    img.attr('alt', title.val());
+                ImagePopup.handler = function() {
+                    ImagePopup.getPicker().hide();
+                    img.attr('src', ImagePopup.image.val());
+                    img.attr('alt', ImagePopup.title.val());
                     marker.replace(img[0].outerHTML);
                 };
             });
@@ -150,25 +164,24 @@ define(['jquery', 'tmpl!image.modal,image.replace', 'uikit', 'finder'], function
                 return marker.found[0];
             }
 
-            if (!finder) {
-                initFinder(options);
-            }
-
             marker.editor.preview.on('click', '#' + marker.uid + ' .js-config', function() {
-                title.val(marker.found[2]);
-                image.val(marker.found[3]);
+
+                ImagePopup.title.val(marker.found[2]);
+                ImagePopup.image.val(marker.found[3]);
 
                 //load finder in image dir
-                finder.loadPath(marker.found[3].trim() && marker.found[3].indexOf(rootpath) === 0 ? marker.found[3].replace(rootpath, '').split('/').slice(0, -1).join('/') : '');
 
-                updatePreview(image.val());
-                goto('settings');
-                picker.show();
-                setTimeout(function() { title.focus(); }, 10);
+                ImagePopup.updatePreview(ImagePopup.image.val());
+                ImagePopup.goto('settings');
+                ImagePopup.getPicker().show();
 
-                handler = function() {
-                    picker.hide();
-                    marker.replace('![' + title.val() + '](' + image.val() + ')');
+                ImagePopup.finder.loadPath(marker.found[3].trim() && marker.found[3].indexOf(rootpath) === 0 ? marker.found[3].replace(rootpath, '').split('/').slice(0, -1).join('/') : '');
+
+                setTimeout(function() { ImagePopup.title.focus(); }, 10);
+
+                ImagePopup.handler = function() {
+                    ImagePopup.getPicker().hide();
+                    marker.replace('![' + ImagePopup.title.val() + '](' + ImagePopup.image.val() + ')');
                 };
             });
 
@@ -178,6 +191,37 @@ define(['jquery', 'tmpl!image.modal,image.replace', 'uikit', 'finder'], function
 
             return tmpl.render('image.replace', { marker: marker, src: ((marker.found[3] && 'http://' !== marker.found[3].trim()) ? marker.found[3] : false), alt: marker.found[2] }).replace(/(\r\n|\n|\r)/gm, '');
         });
+
+        // override default image toolbar command
+        htmleditor.commands.picture.action = function(editor) {
+
+            var $this = this;
+
+            ImagePopup.handler = function() {
+
+                var repl;
+
+                ImagePopup.getPicker().hide();
+
+                if($this.getMode() == 'html') {
+                    repl = '<img src="' + ImagePopup.image.val() + '" alt="' + ImagePopup.title.val() + '">';
+                } else {
+                    repl = '![' + ImagePopup.title.val() + '](' + ImagePopup.image.val() + ')';
+                }
+
+                editor.replaceSelection(repl, 'end');
+            };
+
+            ImagePopup.image.val('');
+            ImagePopup.updatePreview(ImagePopup.image.val());
+            ImagePopup.goto('settings');
+            ImagePopup.getPicker().show();
+            ImagePopup.finder.loadPath();
+
+            setTimeout(function() {
+                ImagePopup.image.focus();
+            }, 10);
+        };
 
         editors.forEach(function(editor) {
             editor.options.plugins.push('htmlimages');
--- extensions/system/assets/js/editor/video.js
+++ extensions/system/assets/js/editor/video.js
@@ -1,72 +1,97 @@
 define(['jquery', 'tmpl!video.modal,video.replace', 'uikit', 'finder'], function($, tmpl, uikit, Finder) {
 
-    var base      = requirejs.toUrl(''),
-        modal     = $(tmpl.render('video.modal')).appendTo('body'),
-        element   = modal.find('.js-finder'),
-        video     = modal.find('.js-url'),
-        preview   = modal.find('.js-video-preview'),
-        btnselect = modal.find('.js-select-image'),
-        screens   = modal.find('[data-screen]').css({'animation-duration':'0.1s', '-webkit-animation-duration':'0.1s'}),
-        goto      = function(screen) {
+    var VideoPopup = {
 
-            var next = screens.filter('[data-screen="'+screen+'"]');
+        init: function(options){
 
-            screens.addClass('uk-hidden')
-            next.removeClass('uk-hidden');
+            var $this = this;
 
-            picker.updateScrollable();
+            this.options   = options;
+
+            this.base      = requirejs.toUrl('');
+            this.modal     = $(tmpl.render('video.modal')).appendTo('body');
+            this.element   = this.modal.find('.js-finder');
+            this.video     = this.modal.find('.js-url');
+            this.preview   = this.modal.find('.js-video-preview');
+            this.btnselect = this.modal.find('.js-select-image');
+            this.screens   = this.modal.find('[data-screen]').css({'animation-duration':'0.1s', '-webkit-animation-duration':'0.1s'});
+            this.finder    = null;
+            this.picker    = null;
+            this.handler   = null;
+
+            // events
+            this.modal.on('click', '.js-update', function() {
+                $this.handler();
+            });
+
+            this.modal.on('click', '[data-goto]', function(e){
+                e.preventDefault();
+                $this.goto($(this).data('goto'));
+            });
+
+            this.element.on('selected-rows', function(e, rows) {
+
+                if (rows.length === 1) {
+
+                    var data = $(rows[0]).data();
+
+                    if (data.type == 'file' && data.url.match(/\.(mpeg|ogv|mp4|webm|wmv)$/i)) {
+                        $this.btnselect.prop('disabled', false).data('url', data.url);
+                    }
+
+                } else {
+                    $this.btnselect.prop('disabled', true);
+                }
+            });
+
+            this.btnselect.on('click', function() {
+
+                var url = $this.btnselect.data('url');
+
+                $this.updatePreview(url);
+
+                // convert to relative urls
+                if (url.indexOf($this.base) === 0) {
+                    url = url.replace($this.base, '');
+                }
+
+                $this.video.val(url);
+                $this.goto('settings');
+            });
         },
-        handler, finder, picker;
 
-    modal.on('click', '.js-update', function() {
-        handler();
-    });
+        getPicker: function() {
 
-    modal.on('click', '[data-goto]', function(e){
-        e.preventDefault();
-        goto($(this).data('goto'));
-    });
+            if(!this.picker) {
 
-    element.on('selected-rows', function(e, rows) {
-
-        if (rows.length === 1) {
-
-            var data = $(rows[0]).data();
-
-            if (data.type == 'file' && data.url.match(/\.(mpeg|ogv|mp4|webm|wmv)$/i)) {
-                btnselect.prop('disabled', false).data('url', data.url);
+                this.finder = new Finder(this.element, this.options);
+                this.element.find('.js-finder-files').addClass('uk-overflow-container');
+                this.picker = new uikit.modal.Modal(this.modal);
+                this.element.find('.js-finder-toolbar-left').prepend(this.btnselect);
             }
 
-        } else {
-            btnselect.prop('disabled', true);
+            return this.picker;
+        },
+
+        goto: function(screen){
+            var next = this.screens.filter('[data-screen="'+screen+'"]');
+
+            this.screens.addClass('uk-hidden');
+            next.removeClass('uk-hidden');
+
+            this.getPicker().updateScrollable();
+        },
+
+        updatePreview: function(url) {
+            // convert to relative urls
+            if (url && !url.match(/^(\/|http\:|https\:|ftp\:)/i)) {
+                url = this.base + '/' + url;
+            }
+
+            this.preview.html(getVideoPreview(url));
         }
-    });
+    };
 
-    btnselect.on('click', function() {
-
-        var url = btnselect.data('url');
-
-        updatePreview(url);
-
-        // convert to relative urls
-        if (url.indexOf(base) === 0) {
-            url = url.replace(base, '');
-        }
-
-        video.val(url);
-
-        goto('settings');
-    });
-
-    function updatePreview(url) {
-
-        // convert to relative urls
-        if (url && !url.match(/^(\/|http\:|https\:|ftp\:)/i)) {
-            url = base + '/' + url;
-        }
-
-        preview.html(getVideoPreview(url));
-    }
 
     function getVideoPreview(url) {
 
@@ -110,6 +135,8 @@ define(['jquery', 'tmpl!video.modal,video.replace', 'uikit', 'finder'], function
 
     return function(htmleditor, options, editors) {
 
+        VideoPopup.init(options);
+
         editors = editors || [];
 
         var rootpath = options.root.replace(/^\/+|\/+$/g, '')+'/';
@@ -120,11 +147,20 @@ define(['jquery', 'tmpl!video.modal,video.replace', 'uikit', 'finder'], function
             label: '<i class="uk-icon-video-camera"></i>',
             action: function(editor) {
 
-                var replace = '(video){"src":"$1"}',
-                    text = editor.getSelection(),
-                    markdown = replace.replace('$1', text);
+                VideoPopup.handler = function() {
+                    VideoPopup.getPicker().hide();
+                    editor.replaceSelection('(video)' + JSON.stringify({ src: VideoPopup.video.val() }), 'end');
+                };
 
-                editor.replaceSelection(markdown, 'end');
+                VideoPopup.video.val('');
+                VideoPopup.updatePreview(VideoPopup.video.val());
+                VideoPopup.goto('settings');
+                VideoPopup.getPicker().show();
+                VideoPopup.finder.loadPath();
+
+                setTimeout(function() {
+                    VideoPopup.video.focus();
+                }, 10);
             }
         };
 
@@ -132,40 +168,29 @@ define(['jquery', 'tmpl!video.modal,video.replace', 'uikit', 'finder'], function
 
         htmleditor.addPlugin('videos', /\(video\)\{(.+?)\}/gim, function(marker) {
 
-            if (!finder) {
-
-                finder = new Finder(element, options);
-                element.find('.js-finder-files').addClass('uk-overflow-container');
-                picker = new uikit.modal.Modal(modal);
-
-                element.find('.js-finder-toolbar-left').prepend(btnselect);
-            }
-
             var data = { src: '' };
 
             try {
-
                 data = $.extend(data, (new Function('', 'var json = {' + marker.found[1] + '}; return JSON.parse(JSON.stringify(json));'))());
-
-            } catch (e) {
-            }
+            } catch (e) {}
 
             marker.editor.preview.on('click', '#' + marker.uid + ' .js-config', function() {
 
-                video.val(data.src);
-                updatePreview(video.val());
-                goto('settings');
-                picker.show();
+                VideoPopup.video.val(data.src);
+                VideoPopup.updatePreview(VideoPopup.video.val());
+                VideoPopup.goto('settings');
+                VideoPopup.getPicker().show();
+
                 setTimeout(function() {
-                    video.focus();
+                    VideoPopup.video.focus();
                 }, 10);
 
-                handler = function() {
-                    picker.hide();
-                    marker.replace('(video)' + JSON.stringify({ src: video.val() }));
+                VideoPopup.handler = function() {
+                    VideoPopup.getPicker().hide();
+                    marker.replace('(video)' + JSON.stringify({ src: VideoPopup.video.val() }));
                 };
 
-                finder.loadPath(data.src.trim() && data.src.indexOf(rootpath) === 0 ? data.src.replace(rootpath, '').split('/').slice(0, -1).join('/') : '');
+                VideoPopup.finder.loadPath(data.src.trim() && data.src.indexOf(rootpath) === 0 ? data.src.replace(rootpath, '').split('/').slice(0, -1).join('/') : '');
             });
 
             marker.editor.preview.on('click', '#' + marker.uid + ' .js-remove', function() {
