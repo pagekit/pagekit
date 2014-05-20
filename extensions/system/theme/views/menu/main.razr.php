fdc5dec338909280aec3c0c8248395ba271b03f9
Artur Heinze <artur@yootheme.com>
2014-05-20 15:19:15 +0200

    cleanup

--- extensions/system/theme/views/menu/main.razr.php
+++ extensions/system/theme/views/menu/main.razr.php
@@ -1,4 +1,4 @@
-<ul class="uk-grid uk-grid-width-1-3" data-uk-grid-margin>
+<ul class="uk-grid uk-grid-width-1-3">
 @foreach (root.children as item)
     <li@( item.attribute('active') ? ' class="uk-active"' )>
         <a href="@url.route(item.url)">
