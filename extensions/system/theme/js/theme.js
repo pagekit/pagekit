db0267e496625a39a39d1c7530a9c14a6d78b57d
Artur Heinze <artur@yootheme.com>
2014-05-20 13:58:58 +0200

    toolbar fixed on scroll

--- extensions/system/theme/js/theme.js
+++ extensions/system/theme/js/theme.js
@@ -9,8 +9,23 @@ jQuery(function($) {
     });
 
     $('.pk-options').each(function() {
+
         $('.tm-navbar').addClass('tm-navbar-margin');
-        $(this).addClass('uk-container uk-container-center').wrap('<div class="tm-toolbar">');
+
+        var toolbar = $(this).addClass('uk-container uk-container-center').wrap('<div class="tm-toolbar">').parent(),
+            offset  = toolbar.offset();
+
+        $(window).on('scroll', (function(){
+
+            var fn = function(){
+                toolbar.css(window.scrollY > offset.top ? {'position': 'fixed', 'top':0} : {'position': '', 'top':''});
+            };
+
+            fn();
+
+            return fn;
+        }));
+
     });
 
     // always fit navbar
