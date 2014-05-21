8aaed69b6ab942de5b3b1c91f7214911511f134e
Sascha Dube <sascha.dube@gmail.com>
2014-05-21 14:18:25 +0200

    updated text widget edit

--- extensions/system/views/widgets/text/edit.razr.php
+++ extensions/system/views/widgets/text/edit.razr.php
@@ -1,6 +1,8 @@
 <div class="uk-form-row">
-    @editor('widget[settings][content]', widget.get('content'), ['id' => 'form-content', 'markdown' => widget.get('markdown', 0)])
-</div>
-<div class="uk-form-row">
-    <label><input type="checkbox" name="widget[settings][markdown]" value="1"@(widget.get('markdown', 0) ? ' checked')> @trans('Enable Markdown')</label>
+    <div class="uk-form-controls">
+        @editor('widget[settings][content]', widget.get('content'), ['id' => 'form-content', 'markdown' => widget.get('markdown', 0)])
+        <p class="uk-form-controls-condensed">
+            <label><input type="checkbox" name="widget[settings][markdown]" value="1"@(widget.get('markdown', 0) ? ' checked')> @trans('Enable Markdown')</label>
+        </p>
+    </div>
 </div>
