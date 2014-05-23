5b4a06e08330cbd07b0f7c467e7bf1e4a3f46978
Sascha Dube <sascha.dube@gmail.com>
2014-05-23 09:34:01 +0200

    added off-canvas to admin theme again

--- extensions/system/theme/templates/template.razr.php
+++ extensions/system/theme/templates/template.razr.php
@@ -17,20 +17,42 @@
 
                 <nav class="uk-navbar">
 
-                    <div class="uk-navbar-content">
+                    <div class="uk-navbar-content uk-hidden-small">
                         <div class="uk-display-inline-block" data-uk-dropdown>
                             <img class="tm-logo" src="@url.to('extension://system/assets/images/pagekit-logo.svg')" width="24" height="29" alt="@trans('Pagekit')">
                             <div class="uk-dropdown tm-dropdown">
-                            @include('extension://system/theme/views/menu/main.razr.php', ['root' => nav])
+                                <ul class="uk-sortable uk-grid uk-grid-width-1-3 js-admin-menu" data-url="@url.route('@system/system/adminmenu')" data-uk-sortable>
+                                @foreach (nav.children as item)
+                                    <li@( item.attribute('active') ? ' class="uk-active"' ) data-id="@item.getId()">
+                                        <a class="uk-panel pk-panel-icon" href="@url.route(item.url)">
+                                            <img src="@url.to(item.getIcon() ?: 'asset://system/images/icon-settings-extensions.svg')" width="50" height="50">
+                                            <p>@trans(item)</p>
+                                        </a>
+                                    </li>
+                                @endforeach
+                                </ul>
                             </div>
                         </div>
                     </div>
 
-                    @include('extension://system/theme/views/menu/subnav.razr.php', ['root' => subnav])
+                    <a class="uk-navbar-content uk-visible-small" href="#offcanvas" data-uk-offcanvas>
+                        <img class="tm-logo" src="@url.to('extension://system/assets/images/pagekit-logo.svg')" width="24" height="29" alt="@trans('Pagekit')">
+                    </a>
+
+                    <ul class="uk-navbar-nav uk-hidden-small">
+                        <li@( subnav.attribute('active') ? ' class="uk-active"' )>
+                            <a href="@url.route(subnav.url)">@trans(subnav)</a>
+                        </li>
+                        @foreach (subnav.children as item)
+                        <li@( item.attribute('active') ? ' class="uk-active"' )>
+                            <a href="@url.route(item.url)">@trans(item)</a>
+                        </li>
+                        @endforeach
+                    </ul>
 
                     <div class="uk-navbar-flip">
 
-                        <div class="uk-navbar-content">
+                        <div class="uk-navbar-content uk-hidden-small">
                             <div class="uk-display-inline-block" data-uk-dropdown>
                                 @set (user = app.users.get())
                                 @gravatar(user.email, ['size' => 72, 'attrs' => ['width' => '36', 'height' => '36', 'class' => 'uk-border-circle', 'alt' => user.username]])
@@ -45,10 +67,14 @@
                             </div>
                         </div>
 
+                        <a class="uk-navbar-content uk-visible-small" href="#offcanvas-flip" data-uk-offcanvas>
+                            @gravatar(user.email, ['size' => 72, 'attrs' => ['width' => '36', 'height' => '36', 'class' => 'uk-border-circle', 'alt' => user.username]])
+                        </a>
+
                     </div>
 
-                    <div class="uk-navbar-center uk-visible-small">
-                        
+                    <div class="uk-navbar-content uk-navbar-center uk-visible-small">
+                        Headline
                     </div>
 
                 </nav>
@@ -59,6 +85,45 @@
         <div class="tm-main uk-container uk-container-center">@action('content')</div>
 
         <div class="uk-hidden">@action('messages')</div>
+
+        <div id="offcanvas" class="uk-offcanvas">
+            <div class="uk-offcanvas-bar">
+
+                <ul class="uk-nav uk-nav-offcanvas">
+                    @if (subnav.children)
+                        <li class="uk-nav-header">@trans(subnav)</li>
+                        <li@( subnav.attribute('active') ? ' class="uk-active"' )>
+                            <a href="@url.route(subnav.url)">@trans(subnav)</a>
+                        </li>
+                        @foreach (subnav.children as item)
+                            <li@(item.attribute('active') ? ' class="uk-active"' )>
+                                <a href="@url.route(item.url)">@trans(item)</a>
+                            </li>
+                        @endforeach
+                    @endif
+                    <li class="uk-nav-header">@trans('Extensions')</li>
+                    @foreach (nav.children as item)
+                        <li@(item.attribute('active') ? ' class="uk-active"')>
+                            <a href="@url.route(item.url)">@trans(item)</a>
+                        </li>
+                    @endforeach
+                </ul>
+
+            </div>
+        </div>
+
+        <div id="offcanvas-flip" class="uk-offcanvas">
+            <div class="uk-offcanvas-bar uk-offcanvas-bar-flip">
+                
+                <ul class="uk-nav uk-nav-offcanvas">
+                    <li class="uk-nav-header">@user.username</li>
+                    <li><a href="@url.base">@trans('Visit Site')</a></li>
+                    <li><a href="@url.route('@system/user/edit', ['id' => user.id])">@trans('Settings')</a></li>
+                    <li><a href="@url.route('@system/auth/logout', ['redirect' => url.route('@system/system/admin', [], true)])">@trans('Logout')</a></li>
+                </ul>
+
+            </div>
+        </div>
 
     </body>
 </html>
