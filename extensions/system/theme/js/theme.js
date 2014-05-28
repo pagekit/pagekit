e15d3f03ede346768ecbeebacdb9594003518eec
Artur Heinze <artur@yootheme.com>
2014-05-28 11:58:33 +0200

    toolbar flickering fixed

--- extensions/system/theme/js/theme.js
+++ extensions/system/theme/js/theme.js
@@ -1,66 +1,86 @@
-jQuery(function($) {
+document.addEventListener("DOMContentLoaded", function(event) {
 
-    $.UIkit.notify.message.defaults.timeout = 2000;
+    (function($, $doc){
 
-    $('.pk-system-messages').children().each(function() {
-        var message = $(this);
-        $.UIkit.notify(message.html(), message.data());
-        message.remove();
-    });
+        // adjust toolbar
 
-    $(document).on('uk-domready', (function() {
+        $doc.on('uk-domready', (function() {
 
-        var i = 0;
+            var navbar = $('.tm-navbar'), bars = [], fn;
 
-        var fn = function() {
+            document.body.style.visibility = 'hidden';
 
-            $('.pk-toolbar:not([toolbar-init])').each(function() {
+            fn = function() {
 
-                var ele = $(this);
+                var toolbars = $('.pk-toolbar:not([toolbar-init])');
 
-                // ignore toolbars in modals
-                if (ele.parents('.uk-modal:first').length) {
-                    return;
+                if (toolbars.length && !navbar.hasClass('tm-navbar-margin')) {
+                    navbar.addClass('tm-navbar-margin');
                 }
 
-                $('.tm-navbar').addClass('tm-navbar-margin');
+                toolbars.each(function() {
 
-                var toolbar = ele.attr('toolbar-init', 'true').addClass('uk-container uk-container-center').wrap('<div class="tm-toolbar">').parent(),
-                    offset  = toolbar.offset();
+                    var ele = $(this);
 
-                $(document).on('uk-scroll', (function(){
+                    // ignore toolbars in modals
+                    if (ele.parents('.uk-modal:first').length) return;
 
-                    var fn = function(){
-                        toolbar.css(window.scrollY > offset.top ? {'position': 'fixed', 'top':0} : {'position': '', 'top':''});
-                    };
+                    var toolbar = ele.addClass('uk-container uk-container-center').wrap('<div class="tm-toolbar">').parent(), offset  = toolbar.offset();
 
-                    fn();
+                    bars.push((function(){
 
-                    return fn;
-                }));
+                        var check = function(){
+                            return toolbar.css(window.scrollY > offset.top ? {'position': 'fixed', 'top':0} : {'position': '', 'top':''}) ? check : check;
+                        };
+                        return check();
+                    })());
+
+                    ele.attr('toolbar-init', 'true');
+                });
+
+                return fn;
+            };
+
+            document.body.style.visibility = '';
+
+            $doc.on('uk-scroll', (function(){
+
+                var onscroll = function(){
+                    return bars.forEach(function(check){ check(); }) ? onscroll : onscroll;
+                };
+
+                return onscroll();
+            }));
+
+            return fn();
+
+        })());
+
+        // show system messages
+
+        $.UIkit.notify.message.defaults.timeout = 2000;
+
+        $('.pk-system-messages').children().each(function() {
+            var message = $(this);
+            $.UIkit.notify(message.html(), message.data());
+            message.remove();
+        });
+
+
+        // save current menu order
+
+        $('.js-admin-menu').on('sortable-stop', function() {
+
+            var data = {};
+
+            $(this).children().each(function(i) {
+                data[$(this).data('id')] = i;
             });
 
-        };
-
-        fn();
-
-        return fn;
-
-    })());
-
-
-
-    $('.js-admin-menu').on('sortable-stop', function() {
-
-        var data = {};
-
-        $(this).children().each(function(i) {
-            data[$(this).data('id')] = i;
+            $.post($(this).data('url'), {'order': data}, function() {
+                // message ?
+            });
         });
 
-        $.post($(this).data('url'), {'order': data}, function() {
-            // message ?
-        });
-    });
-
+    })(jQuery, jQuery(document));
 });
