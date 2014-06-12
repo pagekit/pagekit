ab8658bc24053d5b08cc10ee0567666746e5bca6
Sascha Dube <sascha.dube@gmail.com>
2014-06-12 09:56:33 +0200

    cleanup registration view markup

--- extensions/system/views/user/registration.razr.php
+++ extensions/system/views/user/registration.razr.php
@@ -1,20 +1,20 @@
-@script('user', 'system/js/user/registration.js', 'requirejs')
+@script('form-password', 'vendor://assets/uikit/js/addons/form-password.js', ['jquery', 'uikit'])
 
 <form class="uk-article uk-form uk-form-stacked" action="@url.route('@system/registration/register')" method="post">
 
     <h1 class="uk-article-title">@trans('User Registration')</h1>
 
     <div class="uk-form-row">
-        <label for="form-name" class="uk-form-label">@trans('Name')</label>
+        <label for="form-username" class="uk-form-label">@trans('Username')</label>
         <div class="uk-form-controls">
-            <input id="form-name" class="uk-form-width-large" type="text" name="user[name]" value="" required>
+            <input id="form-username" class="uk-form-width-large" type="text" name="user[username]" value="" required>
         </div>
     </div>
 
     <div class="uk-form-row">
-        <label for="form-username" class="uk-form-label">@trans('Username')</label>
+        <label for="form-name" class="uk-form-label">@trans('Name')</label>
         <div class="uk-form-controls">
-            <input id="form-username" class="uk-form-width-large" type="text" name="user[username]" value="" required>
+            <input id="form-name" class="uk-form-width-large" type="text" name="user[name]" value="" required>
         </div>
     </div>
 
@@ -28,7 +28,10 @@
     <div class="uk-form-row">
         <label for="form-password" class="uk-form-label">@trans('Password')</label>
         <div class="uk-form-controls">
-            <input id="form-password" class="uk-form-width-large" type="password" name="user[password]" value="">
+            <div class="uk-form-password">
+                <input id="form-password" class="uk-form-width-large" type="password" name="user[password]" value="">
+                <a href="" class="uk-form-password-toggle" data-uk-form-password="{ lblShow: '@trans('Show')', lblHide: '@trans('Hide')' }">@trans('Show')</a>
+            </div>
         </div>
     </div>
 
