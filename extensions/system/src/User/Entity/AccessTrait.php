29c65541502243c1d692a68cbce57d1144237d7a
Steffan Schiewe <steffan@schiewe.biz>
2014-07-03 15:29:07 +0200

    Cleanup

--- extensions/system/src/User/Entity/AccessTrait.php
+++ extensions/system/src/User/Entity/AccessTrait.php
@@ -27,20 +27,20 @@ trait AccessTrait
     }
 
     /**
-     * @param  UserInterface $user
-     * @return bool
-     */
-    public function hasAccess(UserInterface $user)
-    {
-        return !$roles = $this->getRoles() or array_intersect(array_keys($user->getRoles()), $roles);
-    }
-
-    /**
      * @param  RoleInterface $role
      * @return bool
      */
     public function hasRole(RoleInterface $role)
     {
         return in_array($role->getId(), $this->getRoles());
+    }
+
+    /**
+     * @param  UserInterface $user
+     * @return bool
+     */
+    public function hasAccess(UserInterface $user)
+    {
+        return !$roles = $this->getRoles() or array_intersect(array_keys($user->getRoles()), $roles);
     }
 }
