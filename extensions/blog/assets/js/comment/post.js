ce015dea05151534c800c40d62f7c80ed8645ba0
Steffan Schiewe <steffan@schiewe.biz>
2014-07-14 20:03:41 +0200

    Cleanup

--- extensions/blog/assets/js/comment/post.js
+++ extensions/blog/assets/js/comment/post.js
@@ -1,40 +1,37 @@
 jQuery(function($) {
 
-    var $comments = $('.js-comments'), $respond = $('.js-respond', $comments), $cancel = $('.js-cancel-reply', $respond), $parent = $('[name="comment[parent_id]"]', $respond);
+    var comments = $('.js-comments'), respond = $('.js-respond', comments), cancel = $('.js-cancel-reply', respond), parent = $('[name="comment[parent_id]"]', respond), storage = window.localStorage || {};
 
-    $comments
+    comments
         .on('click', '.js-reply', function(e) {
             e.preventDefault();
 
-            var $comment = $(this).closest('[data-comment]');
+            var comment = $(this).closest('[data-comment]');
 
-            $respond.appendTo($comment);
-            $parent.val($comment.data('comment'));
-            $cancel.show();
+            respond.appendTo(comment);
+            parent.val(comment.data('comment'));
+            cancel.show();
         })
         .on('click', '.js-cancel-reply', function(e) {
             e.preventDefault();
 
-            $respond.appendTo($comments);
-            $parent.val(0);
-            $cancel.hide();
+            respond.appendTo(comments);
+            parent.val(0);
+            cancel.hide();
         });
 
-    $cancel.hide();
+    cancel.hide();
 
-    if (window['localStorage']) {
+    $('[name="comment[author]"]').val(storage['blog.comments.author']);
+    $('[name="comment[email]"]').val(storage['blog.comments.email']);
+    $('[name="comment[url]"]').val(storage['blog.comments.url']);
 
-        $('[name="comment[author]"]').val(localStorage.getItem('blog.comments.author'));
-        $('[name="comment[email]"]').val(localStorage.getItem('blog.comments.email'));
-        $('[name="comment[url]"]').val(localStorage.getItem('blog.comments.url'));
-
-        $('form', $respond).on('submit', function() {
-            if ($('[name="comment[author]"]').length) {
-                localStorage.setItem('blog.comments.author', $('[name="comment[author]"]').val());
-                localStorage.setItem('blog.comments.email', $('[name="comment[email]"]').val());
-                localStorage.setItem('blog.comments.url', $('[name="comment[url]"]').val());
-            }
-        });
-    }
+    $('form', respond).on('submit', function() {
+        if ($('[name="comment[author]"]').length) {
+            storage['blog.comments.author'] = $('[name="comment[author]"]').val();
+            storage['blog.comments.email'] = $('[name="comment[email]"]').val();
+            storage['blog.comments.url'] = $('[name="comment[url]"]').val();
+        }
+    });
 
 });
