7986935b33a6e0c0dfe54cd54b9608e61ada1bf2
Steffan Schiewe <steffan@schiewe.biz>
2015-03-02 19:08:43 +0100

    cleanup

--- extensions/system/modules/dashboard/views/weather/index.php
+++ extensions/system/modules/dashboard/views/weather/index.php
@@ -4,11 +4,11 @@
     <div class="uk-text-center" v-show="status == 'loading'">
         <i class="uk-icon-medium uk-icon-spinner uk-icon-spin"></i>
     </div>
+    <div class="uk-alert uk-alert-danger" v-show="status == 'error'">{{ 'Unable to retrieve weather data.' | trans }}</div>
     <div class="pk-weather-image">
     	<img class="uk-text-top" v-attr="src: icon">
     	<span class="uk-text-large uk-text-muted pk-weather-temperature">{{ temperature }}</span>
     </div>
     <h1 class="uk-h2 uk-margin-remove">{{ city }}</h1>
     <h2 class="uk-h3 uk-margin-remove uk-text-muted">{{ country }}</h2>
-    <div class="uk-alert uk-alert-danger" v-show="status == 'error'"><?= __('Unable to retrieve weather data.') ?></div>
 </div>
