4026d75fb03ec578addd60a28bdbd68ccb2d3d3a
Steffan Schiewe <steffan@schiewe.biz>
2015-03-13 13:55:52 +0100

    added ES6 templating

--- extensions/system/app/system.js
+++ extensions/system/app/system.js
@@ -261,4 +261,33 @@
 
     };
 
+
+    /**
+     * ES6 templating (Andrea Giammarchi - WTFPL License)
+     */
+
+    String.prototype.template = function (fn, object) {
+
+        var hasTransformer = typeof fn === 'function',
+            prefix = hasTransformer ? '__tpl' + (+new Date) : '',
+            stringify = JSON.stringify,
+            regex = /\$\{([\S\s]*?)\}/g,
+            evaluate = [],
+            i = 0,
+            m;
+
+        while (m = regex.exec(this)) {
+            evaluate.push(stringify(this.slice(i, regex.lastIndex - m[0].length)), prefix + '(' + m[1] + ')');
+            i = regex.lastIndex;
+        }
+
+        evaluate.push(stringify(this.slice(i)));
+
+        // Function is needed to opt out from possible "use strict" directive
+        return Function(prefix, 'with(this)return' + evaluate.join('+')).call(
+            hasTransformer ? object : fn, // the object to use inside the with
+            hasTransformer && fn          // the optional transformer function to use
+        );
+    };
+
 })(jQuery);
