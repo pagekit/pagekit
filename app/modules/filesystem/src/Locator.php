176a6bb7ddfe7d4988661c58ea49603621c76adb
Steffan Schiewe <steffan@schiewe.biz>
2015-03-31 13:34:08 +0200

    cleanup

--- app/modules/filesystem/src/Locator.php
+++ app/modules/filesystem/src/Locator.php
@@ -75,7 +75,7 @@ class Locator
             }
 
             if (($part = substr($file, strlen($prefix))) !== false) {
-                $path .= ltrim(trim($part), '/');
+                $path .= ltrim($part, '/');
             }
 
             if (file_exists($path)) {
