d5271e86f0964e00572f113bfba17d20edfa8941
Steffan Schiewe <steffan@schiewe.biz>
2015-04-08 17:59:08 +0200

    moved package to system

--- app/modules/package/module.php
+++ app/modules/package/module.php
@@ -1,20 +1,10 @@
 <?php
 
-use Pagekit\System\Package\PackageManager;
-use Pagekit\System\Package\Repository\ExtensionRepository;
-use Pagekit\System\Package\Repository\ThemeRepository;
-
 return [
 
     'name' => 'package',
 
     'main' => function ($app) {
-
-        $app['package'] = function ($app) {
-            return (new PackageManager)
-                ->addRepository('extension', new ExtensionRepository($app['path.extensions']))
-                ->addRepository('theme', new ThemeRepository($app['path.themes']));
-        };
 
     },
 
--- app/system/modules/package/module.php
+++ app/system/modules/package/module.php
@@ -1,10 +1,20 @@
 <?php
 
+use Pagekit\System\Package\PackageManager;
+use Pagekit\System\Package\Repository\ExtensionRepository;
+use Pagekit\System\Package\Repository\ThemeRepository;
+
 return [
 
     'name' => 'system/package',
 
     'main' => function ($app) {
+
+        $app['package'] = function ($app) {
+            return (new PackageManager)
+                ->addRepository('extension', new ExtensionRepository($app['path.extensions']))
+                ->addRepository('theme', new ThemeRepository($app['path.themes']));
+        };
 
         $app->on('system.init', function() use ($app) {
             $app['scripts']->register('marketplace', 'app/system/modules/package/app/marketplace.js', 'vue-system');
