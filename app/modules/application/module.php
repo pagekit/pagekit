b93e1874f98573fd21ab4018f447b760e65de3eb
Steffan Schiewe <steffan@schiewe.biz>
2015-04-09 18:10:25 +0200

    cleanup

--- app/modules/application/module.php
+++ app/modules/application/module.php
@@ -33,7 +33,7 @@ return [
 
         ErrorHandler::register(E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR);
 
-        if ($cli = $app->inConsole() or $app['debug']) {
+        if ($app->inConsole() or $app['debug']) {
             ini_set('display_errors', 1);
         }
 
