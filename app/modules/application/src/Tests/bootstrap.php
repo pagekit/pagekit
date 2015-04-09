0b7283331f6bd750cd2503292f08db78895a9659
Steffan Schiewe <steffan@schiewe.biz>
2015-04-09 13:54:10 +0200

    include system/modules unit tests

--- app/modules/application/src/Tests/bootstrap.php
+++ app/modules/application/src/Tests/bootstrap.php
@@ -1,23 +1,23 @@
 <?php
 
 $autoload = [
-    'Pagekit\\Auth\\' => 'auth/src',
-    'Pagekit\\Cookie\\' => 'cookie/src',
-    'Pagekit\\Database\\' => 'database/src',
-    'Pagekit\\Filesystem\\' => 'filesystem/src',
-    'Pagekit\\Filter\\' => 'filter/src',
-    'Pagekit\\Migration\\' => 'migration/src',
-    'Pagekit\\Option\\' => 'option/src',
-    'Pagekit\\Package\\' => 'package/src',
-    'Pagekit\\Routing\\' => 'routing/src',
-    'Pagekit\\Session\\' => 'session/src',
-    'Pagekit\\Tree\\' => 'tree/src',
-    'Pagekit\\View\\' => 'view/src'
+    'Pagekit\\Auth\\' => '/modules/auth/src',
+    'Pagekit\\Cookie\\' => '/modules/cookie/src',
+    'Pagekit\\Database\\' => '/modules/database/src',
+    'Pagekit\\Filesystem\\' => '/modules/filesystem/src',
+    'Pagekit\\Filter\\' => '/modules/filter/src',
+    'Pagekit\\Migration\\' => '/modules/migration/src',
+    'Pagekit\\Option\\' => '/system/modules/option/src',
+    'Pagekit\\Package\\' => '/modules/package/src',
+    'Pagekit\\Routing\\' => '/modules/routing/src',
+    'Pagekit\\Session\\' => '/modules/session/src',
+    'Pagekit\\Tree\\' => '/modules/tree/src',
+    'Pagekit\\View\\' => '/modules/view/src'
 ];
 
 $path = realpath(__DIR__.'/../../../../');
 $loader = require $path.'/autoload.php';
 
 foreach ($autoload as $namespace => $src) {
-    $loader->addPsr4($namespace, $path.'/modules/'.$src);
+    $loader->addPsr4($namespace, $path.$src);
 }
