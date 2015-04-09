eff4e7bf4f0abf205a4b7eb6bf211d0d62e157d8
Steffan Schiewe <steffan@schiewe.biz>
2015-04-09 14:56:04 +0200

    use lazy asset dependency

--- app/system/modules/finder/module.php
+++ app/system/modules/finder/module.php
@@ -11,10 +11,10 @@ return [
         $app->on('system.init', function() use ($app) {
 
             $app['view']->addHelper(new FinderHelper());
-            $app['scripts']->register('finder', 'app/system/modules/finder/app/finder.js', ['vue-system', 'uikit-upload', 'finder-main']);
-            $app['scripts']->register('finder-main', 'app/system/modules/finder/views/main.php', ['finder-table', 'finder-thumbnail'], 'template');
-            $app['scripts']->register('finder-table', 'app/system/modules/finder/views/table.php', [], 'template');
-            $app['scripts']->register('finder-thumbnail', 'app/system/modules/finder/views/thumbnail.php', [], 'template');
+            $app['scripts']->register('finder', 'app/system/modules/finder/app/finder.js', ['vue-system', 'uikit-upload']);
+            $app['scripts']->register('finder-main', 'app/system/modules/finder/views/main.php', '~finder', 'template');
+            $app['scripts']->register('finder-table', 'app/system/modules/finder/views/table.php', '~finder', 'template');
+            $app['scripts']->register('finder-thumbnail', 'app/system/modules/finder/views/thumbnail.php', '~finder', 'template');
 
         });
 
--- app/system/modules/package/module.php
+++ app/system/modules/package/module.php
@@ -17,10 +17,10 @@ return [
         };
 
         $app->on('system.init', function() use ($app) {
-            $app['scripts']->register('marketplace', 'app/system/modules/package/app/marketplace.js', ['vue-system', 'marketplace-tmpl']);
-            $app['scripts']->register('marketplace-tmpl', 'app/system/modules/package/views/marketplace.php', [], 'template');
-            $app['scripts']->register('upload', 'app/system/modules/package/app/upload.js', ['vue-system', 'uikit-upload', 'upload-tmpl']);
-            $app['scripts']->register('upload-tmpl', 'app/system/modules/package/views/upload.php', [], 'template');
+            $app['scripts']->register('marketplace', 'app/system/modules/package/app/marketplace.js', 'vue-system');
+            $app['scripts']->register('marketplace-tmpl', 'app/system/modules/package/views/marketplace.php', '~marketplace', 'template');
+            $app['scripts']->register('upload', 'app/system/modules/package/app/upload.js', ['vue-system', 'uikit-upload']);
+            $app['scripts']->register('upload-tmpl', 'app/system/modules/package/views/upload.php', '~upload', 'template');
         });
 
     },
