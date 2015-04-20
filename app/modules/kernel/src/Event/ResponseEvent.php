0964fb80b1c8e16eceb77e4f03f156b9156440c4
Steffan Schiewe <steffan@schiewe.biz>
2015-04-20 13:40:32 +0200

    cleanup

--- app/modules/kernel/src/Event/ResponseEvent.php
+++ app/modules/kernel/src/Event/ResponseEvent.php
@@ -12,20 +12,6 @@ class ResponseEvent extends KernelEvent
     protected $response;
 
     /**
-     * Constructor.
-     *
-     * @param string   $name
-     * @param int      $requestType
-     * @param Response $response
-     */
-    public function __construct($name, $requestType, Response $response = null)
-    {
-        parent::__construct($name, $requestType);
-
-        $this->response = $response;
-    }
-
-    /**
      * Checks if a response was set.
      *
      * @return bool
