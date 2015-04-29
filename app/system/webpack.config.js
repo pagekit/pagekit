8431beed559cb6564308e2212d56d2a6823746ec
Steffan Schiewe <steffan@schiewe.biz>
2015-04-29 16:14:53 +0200

    cleanup

--- app/system/webpack.config.js
+++ app/system/webpack.config.js
@@ -1,17 +1,21 @@
+var assets = __dirname + "/../../vendor/assets";
+
 module.exports = [
 
   {
+    context: __dirname,
     entry: {
-      "app/bundle/system": __dirname + "/app/system",
-      "modules/editor/app/bundle/editor": __dirname + "/modules/editor/app/editor",
-      "modules/finder/app/bundle/finder": __dirname + "/modules/finder/app/components/finder.vue",
-      "modules/package/app/bundle/extensions": __dirname + "/modules/package/app/extensions",
-      "modules/package/app/bundle/themes": __dirname + "/modules/package/app/themes",
-      "modules/package/app/bundle/marketplace": __dirname + "/modules/package/app/components/marketplace.vue",
-      "modules/package/app/bundle/upload": __dirname + "/modules/package/app/components/upload.vue"
+      "app/bundle/system": "./app/system",
+      "modules/editor/app/bundle/editor": "./modules/editor/app/editor",
+      "modules/finder/app/bundle/finder": "./modules/finder/app/components/finder.vue",
+      "modules/package/app/bundle/extensions": "./modules/package/app/extensions",
+      "modules/package/app/bundle/themes": "./modules/package/app/themes",
+      "modules/package/app/bundle/marketplace": "./modules/package/app/components/marketplace.vue",
+      "modules/package/app/bundle/upload": "./modules/package/app/components/upload.vue"
     },
     output: {
-      filename: __dirname + "/[name].js"
+      path: __dirname,
+      filename: "./[name].js"
     },
     externals: {
       "lodash": "_",
@@ -28,21 +32,23 @@ module.exports = [
   },
 
   {
+    context: __dirname,
     entry: {
-      "globalize": __dirname + "/app/globalize"
+      "globalize": "./app/globalize"
     },
     output: {
-      filename: __dirname + "/app/bundle/[name].js",
+      path: __dirname,
+      filename: "./app/bundle/[name].js",
       library: "Globalize"
     },
     resolve: {
       alias: {
-        "cldr$": __dirname + "/../../vendor/assets/cldrjs/dist/cldr.js",
-        "cldr/event$": __dirname + "/../../vendor/assets/cldrjs/dist/cldr/event.js",
-        "cldr/supplemental$": __dirname + "/../../vendor/assets/cldrjs/dist/cldr/supplemental.js",
-        "globalize$": __dirname + "/../../vendor/assets/globalize/dist/globalize.js",
-        "globalize/number$": __dirname + "/../../vendor/assets/globalize/dist/globalize/number.js",
-        "globalize/date$": __dirname + "/../../vendor/assets/globalize/dist/globalize/date.js"
+        "cldr$": assets + "/cldrjs/dist/cldr.js",
+        "cldr/event$": assets + "/cldrjs/dist/cldr/event.js",
+        "cldr/supplemental$": assets + "/cldrjs/dist/cldr/supplemental.js",
+        "globalize$": assets + "/globalize/dist/globalize.js",
+        "globalize/number$": assets + "/globalize/dist/globalize/number.js",
+        "globalize/date$": assets + "/globalize/dist/globalize/date.js"
       }
     }
   }
