e688252c609e5e624e6797c7627f4c59d6b6cb0c
Steffan Schiewe <steffan@schiewe.biz>
2015-04-30 11:14:06 +0200

    use path.join

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var glob = require("glob");
 var _ = require("lodash");
+var glob = require("glob");
 var path = require("path");
 
 var exports = [
@@ -16,7 +16,7 @@ var exports = [
 ];
 
 glob.sync('{app/modules/**,app/system/**,extensions/**,themes/**}/webpack.config.js').forEach(function (file) {
-    var dir = __dirname + '/' + path.dirname(file);
+    var dir = path.join(__dirname, path.dirname(file));
     exports = exports.concat(require('./' + file).map(function (config) {
         return _.merge({ context: dir, output: { path: dir } }, config);
     }));
