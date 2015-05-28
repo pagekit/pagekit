f72a9a2413b21ab6a90b3090b344f90c4c8bae6e
Sascha Dube <sascha.dube@gmail.com>
2015-05-28 09:01:42 +0200

    updated site view markup

--- app/system/modules/site/app/components/menus.vue
+++ app/system/modules/site/app/components/menus.vue
@@ -3,7 +3,7 @@
     <div class="uk-panel" v-repeat="menu: menus">
 
         <div class="uk-panel-badge" data-uk-dropdown="{ mode: 'click' }">
-            <a v-on="click: $event.preventDefault()"><i class="uk-icon uk-icon-plus"></i></a>
+            <a class="uk-link-muted" v-on="click: $event.preventDefault()"><i class="uk-icon uk-icon-plus"></i></a>
             <div class="uk-dropdown uk-dropdown-small">
                 <ul class="uk-nav uk-nav-dropdown">
                     <li v-repeat="type: types | unmounted"><a v-on="click: add(menu, type)">{{ type.label }}</a></li>
@@ -11,36 +11,45 @@
             </div>
         </div>
 
-        <h3 class="uk-panel-title uk-flex-item-1" v-on="click: edit(menu)">{{ menu.label }}</h3>
+        <h3 class="uk-panel-title" v-on="click: edit(menu)">{{ menu.label }}</h3>
 
         <node-list></node-list>
 
     </div>
 
     <p>
-        <a v-on="click: edit()"><i class="uk-icon-th-list"></i> {{ 'Create Menu' | trans }}</a>
+        <a class="uk-button uk-width-1-1" v-on="click: edit()"><i class="uk-icon-th-list"></i> {{ 'Create Menu' | trans }}</a>
     </p>
 
     <div class="uk-modal" v-el="modal">
 
-        <div class="uk-modal-dialog" v-if="menu">
+        <form class="uk-modal-dialog uk-form uk-form-stacked" name="menuForm" v-on="valid: save" v-if="menu">
 
-            <form name="menuForm" v-on="valid: save">
+            <div class="uk-modal-header">
+                <h2>{{ 'Add Menu' | trans }}</h2>
+            </div>
 
-                <p>
-                    <input class="uk-width-1-1 uk-form-large" name="label" type="text" placeholder="{{ 'Enter Menu Name' | trans }}" v-model="menu.label" v-valid="alphaNum">
-                    <span class="uk-form-help-block uk-text-danger" v-show="menuForm.label.invalid">{{ 'Invalid name.' | trans }}</span>
-                </p>
-                <p>
-                    <input class="uk-width-1-1 uk-form-large" name="id" type="text" placeholder="{{ 'Enter Menu Slug' | trans }}" v-model="menu.id" v-valid="alphaNum, unique">
-                    <span class="uk-form-help-block uk-text-danger" v-show="menuForm.id.invalid">{{ 'Invalid slug.' | trans }}</span>
-                </p>
+            <div class="uk-form-row">
+                <label for="form-name" class="uk-form-label">{{ 'Name' | trans }}</label>
+                <div class="uk-form-controls">
+                    <input id="form-name" class="uk-width-1-1 uk-form-large" name="label" type="text" v-model="menu.label" v-valid="alphaNum">
+                    <p class="uk-form-help-block uk-text-danger" v-show="menuForm.label.invalid">{{ 'Invalid name.' | trans }}</p>
+                </div>
+            </div>
 
-                <button class="uk-button uk-button-primary" v-attr="disabled: menuForm.invalid">{{ 'Save' | trans }}</button>
-                <button class="uk-button uk-modal-close" v-on="click: cancel">{{ 'Cancel' | trans }}</button>
-                <button class="uk-button uk-button-danger uk-float-right" v-show="menu.oldId" v-on="click: delete">{{ 'Delete' | trans }}</button>
+            <div class="uk-form-row">
+                <label for="form-slug" class="uk-form-label">{{ 'Slug' | trans }}</label>
+                <div class="uk-form-controls">
+                    <input id="form-slug" class="uk-width-1-1 uk-form-large" name="id" type="text" v-model="menu.id" v-valid="alphaNum, unique">
+                    <p class="uk-form-help-block uk-text-danger" v-show="menuForm.id.invalid">{{ 'Invalid slug.' | trans }}</p>
+                </div>
+            </div>
 
-            </form>
+            <div class="uk-modal-footer uk-text-right">
+                <button class="uk-button uk-button-link" v-show="menu.oldId" v-on="click: delete">{{ 'Delete' | trans }}</button>
+                <button class="uk-button uk-button-link uk-modal-close" v-on="click: cancel">{{ 'Cancel' | trans }}</button>
+                <button class="uk-button uk-button-link" v-attr="disabled: menuForm.invalid">{{ 'Save' | trans }}</button>
+            </div>
 
         </div>
 
