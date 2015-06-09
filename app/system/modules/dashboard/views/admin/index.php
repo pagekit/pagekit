c24550f9627216b4f69a0261a38539b8a2bf78de
Sascha Dube <sascha.dube@gmail.com>
2015-06-09 17:51:09 +0200

    updated dashboard markup

--- app/system/modules/dashboard/views/admin/index.php
+++ app/system/modules/dashboard/views/admin/index.php
@@ -2,6 +2,19 @@
 
 <div id="dashboard">
 
+    <div class="uk-margin uk-flex uk-flex-right" data-uk-margin>
+        <div class="uk-button-dropdown" data-uk-dropdown="{ mode: 'click' }">
+            <a class="uk-button uk-button-primary" v-on="click: $event.preventDefault()">{{ 'Add Widget' || trans }}</a>
+            <div class="uk-dropdown uk-dropdown-small uk-dropdown-flip">
+                <ul class="uk-nav uk-nav-dropdown">
+                    <li v-repeat="type: getTypes()">
+                        <a v-on="click: add(type)">{{ type.label }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
     <div class="uk-grid pk-grid-small uk-grid-medium" data-uk-grid-margin>
         <div class="uk-width-medium-1-3" v-repeat="column: columns">
 
@@ -9,9 +22,9 @@
 
                 <div v-component="widget-panel" inline-template>
 
-                    <div class="uk-position-top-right uk-margin-small-top uk-margin-small-right uk-text-large">
-                        <a class="uk-invisible uk-icon-hover uk-icon-remove" title="{{ 'Delete' | trans }}" v-on="click: remove()"></a>
-                        <a class="uk-invisible uk-icon-hover uk-icon-pencil" title="{{ 'Edit' | trans }}" v-on="click: edit()" v-if="type.editable !== false"></a>
+                    <div class="uk-panel-badge">
+                        <a class="uk-icon-remove uk-icon-hover uk-hidden" title="{{ 'Delete' | trans }}" v-on="click: remove()"></a>
+                        <a class="uk-icon-cog uk-icon-hover uk-hidden" title="{{ 'Settings' | trans }}" v-on="click: edit()" v-if="type.editable !== false"></a>
                     </div>
 
                     <div v-component="{{ component }}" v-with="widget: widget, editing: isEditing"></div>
@@ -20,19 +33,6 @@
 
             </div>
 
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <div class="uk-button-dropdown" data-uk-dropdown="{ mode: 'click' }">
-            <a class="uk-button" v-on="click: $event.preventDefault()">{{ 'Add Widget' || trans }}</a>
-            <div class="uk-dropdown uk-dropdown-small">
-                <ul class="uk-nav uk-nav-dropdown">
-                    <li v-repeat="type: getTypes()">
-                        <a v-on="click: add(type)">{{ type.label }}</a>
-                    </li>
-                </ul>
-            </div>
         </div>
     </div>
 
