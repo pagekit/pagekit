ea3b31fb3bfa07a928cd20b6c4fb7be3a6d2cc72
Anne <anne@yootheme.com>
2015-06-11 15:38:43 +0200

    fixed blog post view status

--- extensions/blog/views/admin/index.php
+++ extensions/blog/views/admin/index.php
@@ -62,12 +62,11 @@
                     </td>
                     <td class="uk-text-center">
                         <a title="{{ getStatusText(post) }}" v-class="
-                                pk-icon-circle:   post.status == 0,
+                                pk-icon-circle: post.status == 0,
                                 pk-icon-circle-warning: post.status == 1,
-                                pk-icon-circle-success: post.status == 2,
-                                pk-icon-circle-danger:  post.status == 3,
-                                pk-icon-circle-primary:  post.status != 2 || !post.isPublished,
-                                pk-icon-schedule: post.status == 2 && post.isPublished
+                                pk-icon-circle-success: post.status == 2 && post.isPublished,
+                                pk-icon-circle-danger: post.status == 3,
+                                pk-icon-schedule: post.status == 2 && !post.isPublished
                             " v-on="click: toggleStatus(post)"></a>
                     </td>
                     <td>
