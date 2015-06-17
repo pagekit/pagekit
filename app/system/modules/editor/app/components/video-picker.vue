10ec749be0e63150793be1b8b3e7fefb5702a43b
Sascha Dube <sascha.dube@gmail.com>
2015-06-17 16:07:10 +0200

    cleanup video picker markup

--- app/system/modules/editor/app/components/video-picker.vue
+++ app/system/modules/editor/app/components/video-picker.vue
@@ -1,21 +1,22 @@
 <template>
 
     <div class="uk-modal" v-el="modal">
-        <div class="uk-modal-dialog uk-modal-dialog-large uk-form" v-class="uk-modal-dialog-large: view == 'finder'">
+        <div class="uk-modal-dialog uk-form uk-form-stacked" v-class="uk-modal-dialog-large: view == 'finder'">
 
             <div v-show="view == 'settings'">
-                <h1 class="uk-h3">{{ 'Video' | trans }}</h1>
+                
+                <div class="uk-modal-header">
+                    <h2>{{ 'Add Video' | trans }}</h2>
+                </div>
 
-
-
-                <a class="uk-placeholder uk-text-center uk-display-block uk-margin-remove" v-on="click: openFinder" v-if="!video.src">
+                <a class="uk-placeholder uk-text-center uk-display-block" v-on="click: openFinder" v-if="!video.src">
                     <img width="60" height="60" alt="{{ 'Placeholder Image' | trans }}" v-attr="src: $url.static('app/system/assets/images/placeholder-video.svg')">
-                    <p class="uk-text-muted uk-margin-small-top">{{ 'Select image' | trans }}</p>
+                    <p class="uk-text-muted uk-margin-small-top">{{ 'Select video' | trans }}</p>
                 </a>
 
-                <div class="uk-overlay uk-overlay-hover pk-image-max-height uk-flex uk-flex-center uk-flex-middle" v-if="video.src">
+                <div class="uk-overlay uk-overlay-hover pk-image-max-height uk-flex uk-flex-center uk-flex-middle uk-margin" v-if="video.src">
                     <video-view src="{{ video.src }}"></video-view>
-                    <div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-flex uk-flex-center uk-flex-middle uk-text-center">
+                    <div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-flex uk-flex-center uk-flex-middle pk-overlay-border">
                         <div>
                             <a class="pk-icon-edit pk-icon-hover" title="{{ 'Edit' | trans }}" data-uk-tooltip="{delay: 500}" v-on="click: openFinder"></a>
                         </div>
@@ -26,19 +27,27 @@
                     <input type="text" class="uk-width-1-1" placeholder="{{ 'URL' | trans }}" v-model="video.src" lazy>
                 </div>
 
-                <div class="uk-form-row uk-margin-top uk-text-right">
+                <div class="uk-modal-footer uk-text-right">
                     <button class="uk-button uk-button-link uk-modal-close" type="button">{{ 'Cancel' | trans }}</button>
-                    <button class="uk-button uk-button-link uk-button-primary uk-modal-close" type="button" v-on="click: update">{{ 'Update' | trans }}</button>
+                    <button class="uk-button uk-button-link uk-modal-close" type="button" v-on="click: update">{{ 'Update' | trans }}</button>
                 </div>
+
             </div>
 
             <div v-if="view == 'finder'">
-                <h1 class="uk-h3">{{ 'Select Video' | trans }}</h1>
+
                 <v-finder root="{{ finder.root }}" v-ref="finder"></v-finder>
+
                 <div class="uk-margin-top">
                     <button class="uk-button uk-button-primary" type="button" v-attr="disabled: !finder.select" v-on="click: closeFinder(finder.select)">{{ 'Select' | trans }}</button>
                     <button class="uk-button" type="button" v-on="click: closeFinder(false)">{{ 'Cancel' | trans }}</button>
                 </div>
+
+                <div class="uk-modal-footer uk-text-right">
+                    <button class="uk-button uk-button-link" type="button" v-on="click: closeFinder(false)">{{ 'Cancel' | trans }}</button>
+                    <button class="uk-button uk-button-link" type="button" v-attr="disabled: !finder.select" v-on="click: closeFinder(finder.select)">{{ 'Select' | trans }}</button>
+                </div>
+
             </div>
 
         </div>
