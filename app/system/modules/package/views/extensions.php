f9dd04bbdc689012f1954d02372fbbd5af06324b
Anne <anne@yootheme.com>
2015-06-18 18:30:00 +0200

    updated icons + permission icon within extension view

--- app/system/modules/package/views/extensions.php
+++ app/system/modules/package/views/extensions.php
@@ -57,7 +57,7 @@
                         <div class="uk-invisible">
                             <ul class="uk-subnav pk-subnav-icon">
                                 <li><a class="pk-icon-info pk-icon-hover" title="{{ 'View Details' | trans }}" data-uk-tooltip="{delay: 500}"></a></li>
-                                <li v-show="pkg.enabled && pkg.permissions"><a class="pk-icon-users pk-icon-hover" title="{{ 'View Permissions' | trans }}" data-uk-tooltip="{delay: 500}" v-attr="href: $url('admin/user/permissions#:name',{name:pkg.name})"></a></li>
+                                <li v-show="pkg.enabled && pkg.permissions"><a class="pk-icon-permission pk-icon-hover" title="{{ 'View Permissions' | trans }}" data-uk-tooltip="{delay: 500}" v-attr="href: $url('admin/user/permissions#:name',{name:pkg.name})"></a></li>
                                 <li v-show="!pkg.enabled"><a class="pk-icon-delete pk-icon-hover" title="{{ 'Delete' | trans }}" data-uk-tooltip="{delay: 500}" v-on="click: uninstall(pkg)" v-confirm="'Uninstall extension?'"></a></li>
                             </ul>
                         </div>
