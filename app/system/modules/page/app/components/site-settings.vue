b7c21ffa8e4cc8271a8867b9685a958a5033f278
Sascha Dube <sascha.dube@gmail.com>
2015-06-19 17:14:46 +0200

    moved page content into page settings (markup)

--- app/system/modules/page/app/components/site-settings.vue
+++ app/system/modules/page/app/components/site-settings.vue
@@ -1,6 +1,37 @@
 <template>
 
-    <div class="uk-form-row uk-form-horizontal" v-partial="settings"></div>
+    <div class="uk-grid pk-grid-large uk-form-stacked" data-uk-grid-margin>
+        <div class="uk-flex-item-1">
+
+            <div class="uk-form-row">
+                <input class="uk-width-1-1 uk-form-large" type="text" name="page[title]" placeholder="{{ 'Enter Title' | trans }}" v-model="page.title">
+            </div>
+
+            <div class="uk-form-row">
+                <v-editor name="page[content]" value="{{@ page.content }}" options="{{ {markdown : page.data.markdown} }}"></v-editor>
+            </div>
+
+        </div>
+        <div class="pk-width-sidebar">
+
+            <div class="uk-panel">
+
+                <div class="uk-form-row" v-partial="settings"></div>
+
+                <div class="uk-form-row">
+                    <span class="uk-form-label">{{ 'Options' | trans }}</span>
+                    <div class="uk-form-controls">
+                        <label><input type="checkbox" name="page[data][title]" v-model="page.data.title"> {{ 'Show Title' | trans }}</label>
+                    </div>
+                    <div class="uk-form-controls">
+                        <label><input type="checkbox" name="page[data][markdown]" v-model="page.data.markdown"> {{ 'Enable Markdown' | trans }}</label>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 
 </template>
 
