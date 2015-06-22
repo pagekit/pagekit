34999b2e3628e171eadf112746880989200ea992
Malte <malte@yootheme.com>
2015-06-22 16:32:32 +0200

    made Vue filter 'relativeDate' available as instance method '$relativeDate'

--- app/system/app/lib/filters.js
+++ app/system/app/lib/filters.js
@@ -1,18 +1,18 @@
 module.exports = function (Vue) {
 
-    Vue.filter('baseUrl', function(url) {
+    Vue.filter('baseUrl', function (url) {
         return _.startsWith(url, Vue.url.options.root) ? url.substr(Vue.url.options.root.length) : url;
     });
 
-    Vue.filter('trans', function(id, parameters, domain, locale) {
+    Vue.filter('trans', function (id, parameters, domain, locale) {
         return this.$trans(id, parameters, domain, locale);
     });
 
-    Vue.filter('transChoice', function(id, number, parameters, domain, locale) {
+    Vue.filter('transChoice', function (id, number, parameters, domain, locale) {
         return this.$transChoice(id, number, parameters, domain, locale);
     });
 
-    Vue.filter('date', function(date, format) {
+    Vue.filter('date', function (date, format) {
         return this.$date(date, format);
     });
 
@@ -21,9 +21,9 @@ module.exports = function (Vue) {
 
             var op = collection[key];
             if (typeof op === 'string') {
-                return { text: op, value: key };
+                return {text: op, value: key};
             } else {
-                return { label: key, options: toOptions(op) };
+                return {label: key, options: toOptions(op)};
             }
 
         }) : [];
@@ -37,46 +37,12 @@ module.exports = function (Vue) {
 
     });
 
-    Vue.filter('relativeDate', function(value, reference) {
-
-        var SECOND = 1000,
-            MINUTE = 60 * SECOND,
-            HOUR = 60 * MINUTE,
-            DAY = 24 * HOUR,
-            WEEK = 7 * DAY,
-            YEAR = DAY * 365,
-            MONTH = YEAR / 12;
-
-            var formats = [
-
-                [ 1.5 * MINUTE, 'minute',MINUTE ],
-                [ 60 * MINUTE, 'minute', MINUTE ],
-                [ DAY, 'hour', HOUR ],
-                [ 7 * DAY, 'day', DAY ],
-                [ MONTH, 'week', WEEK ]
-
-            ],formatter;
-
-        if (typeof(value)) value = new Date(value);
-        if (!reference) reference = (new Date).getTime();
-        if (reference instanceof Date) reference = reference.getTime();
-        if (value instanceof Date) value = value.getTime();
-
-        var delta = reference - value, format, i, len;
-
-        for (i = -1, len=formats.length; ++i < len; ){
-
-            format = formats[i];
-
-            if (delta < format[0]){
-
-                formatter = Globalize.relativeTimeFormatter( format[1] );
-
-                return formatter(Math.round(delta/format[2])*-1);
-            }
+    Vue.filter('relativeDate', function (value, reference) {
+        try {
+            return this.$relativeDate(value, reference);
+        } catch (e) {
+            return 'NaN';
         }
-
-        return this.$date((new Date(value)).toISOString(), 'medium');
     });
 
 };
--- app/system/app/lib/trans.js
+++ app/system/app/lib/trans.js
@@ -2,7 +2,7 @@ module.exports = function (Vue) {
 
     var formats = ['full', 'long', 'medium', 'short'];
 
-    Vue.prototype.$date = function(date, format) {
+    Vue.prototype.$date = function (date, format) {
 
         var options = format;
 
@@ -19,6 +19,47 @@ module.exports = function (Vue) {
         }
 
         return Globalize.formatDate(date, options);
+    };
+
+    Vue.prototype.$relativeDate = function (value, reference) {
+        var SECOND = 1000,
+            MINUTE = 60 * SECOND,
+            HOUR = 60 * MINUTE,
+            DAY = 24 * HOUR,
+            WEEK = 7 * DAY,
+            YEAR = DAY * 365,
+            MONTH = YEAR / 12;
+
+        var formats = [
+
+            [1.5 * MINUTE, 'minute', MINUTE],
+            [60 * MINUTE, 'minute', MINUTE],
+            [DAY, 'hour', HOUR],
+            [7 * DAY, 'day', DAY],
+            [MONTH, 'week', WEEK]
+
+        ], formatter;
+
+        if (typeof(value)) value = new Date(value);
+        if (!reference) reference = (new Date).getTime();
+        if (reference instanceof Date) reference = reference.getTime();
+        if (value instanceof Date) value = value.getTime();
+
+        var delta = reference - value, format, i, len;
+
+        for (i = -1, len = formats.length; ++i < len;) {
+
+            format = formats[i];
+
+            if (delta < format[0]) {
+
+                formatter = Globalize.relativeTimeFormatter(format[1]);
+
+                return formatter(Math.round(delta / format[2]) * -1);
+            }
+        }
+
+        return this.$date((new Date(value)).toISOString(), 'medium');
     };
 
     Vue.prototype.$trans = Globalize.trans;
