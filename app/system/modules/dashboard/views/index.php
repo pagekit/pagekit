eee4342b9d725b8dcb7911923d7194eba821e03a
Sascha Dube <sascha.dube@gmail.com>
2015-06-25 19:03:47 +0200

    minor markup cleanup

--- app/system/modules/dashboard/views/index.php
+++ app/system/modules/dashboard/views/index.php
@@ -15,7 +15,7 @@
         </div>
     </div>
 
-    <div class="uk-grid pk-grid-small uk-grid-medium uk-grid-match" data-uk-grid-margin>
+    <div class="uk-grid uk-grid-medium uk-grid-match" data-uk-grid-margin>
         <div class="uk-width-medium-1-3" v-repeat="[0,1,2]">
 
             <ul class="uk-sortable pk-sortable" data-column="{{ $index }}">
