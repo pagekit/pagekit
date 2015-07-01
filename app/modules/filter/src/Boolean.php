f5b5e53b353959a04de878ce4f0d1e3f779a7b5c
janschoenherr <jan@yootheme.com>
2015-07-01 14:15:16 +0200

    fixed Boolean filter

--- app/modules/filter/src/Boolean.php
+++ app/modules/filter/src/Boolean.php
@@ -10,12 +10,11 @@ class Boolean extends AbstractFilter
     /**
      * Returns the filtered value.
      *
-     * @param  mixed  $value
-     * 
+     * @param  mixed $value
      * @return bool
      */
     public function filter($value)
     {
-        return (bool) ((string) $value);
+        return (bool) @strval($value);
     }
 }
