545acec6ea7e197f76aa0bdfa4f61ea0d4cd6e5b
Artur Heinze <artur@yootheme.com>
2015-07-09 12:39:22 +0200

    lazy background directive added

---
+++ app/system/app/directives/lazy-background.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    update: function (value) {
+
+        var el = $(this.el), img = new Image();
+
+        img.onload = function() {
+            el.css('background-image', "url('"+value+"')");
+        };
+
+        img.src = encodeURI(value);
+    }
+
+};
--- app/system/app/vue.js
+++ app/system/app/vue.js
@@ -28,6 +28,7 @@ function install (Vue) {
     Vue.directive('confirm', require('./directives/confirm'));
     Vue.directive('gravatar', require('./directives/gravatar'));
     Vue.directive('order', require('./directives/order'));
+    Vue.directive('lazy-background', require('./directives/lazy-background'));
 
     /**
      * Resource
