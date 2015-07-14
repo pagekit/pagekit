74685f22cb28678bc42be4237d88d3d3d45a3993
Florian Letsch <florian@yootheme.com>
2015-07-14 09:50:57 +0200

    fixed view path

--- themes/alpha/views/position-offcanvas.php
+++ themes/alpha/views/position-offcanvas.php
@@ -5,6 +5,6 @@ foreach ($widgets as $widget) {
     if ($widget->getType() == 'widget.menu') {
         echo $widget->render(array_merge(['layout' => 'app/modules/menu/views/widgets/menu/nav.razr', 'classes' => 'uk-nav-offcanvas'], $options));
     } else {
-        $view->render('alpha:views/position.panel.php', ['widgets' => [$widget]]);
+        $view->render('alpha:views/position-panel.php', ['widgets' => [$widget]]);
     }
 }
