3579fee533c15734df0acaffa4a1d4877e5a9ddc
Anne <anne@yootheme.com>
2015-07-21 14:58:28 +0200

    added flex-middle + cleanup span

--- app/system/modules/dashboard/app/components/widget-location.vue
+++ app/system/modules/dashboard/app/components/widget-location.vue
@@ -46,9 +46,9 @@
         <h1 class="uk-margin-large-top uk-margin-small-bottom uk-text-center pk-text-xlarge" v-if="time">{{ time | date format }}</h1>
 
         <h2 class="uk-text-center uk-h4 uk-margin-remove" v-if="time">{{ time | date 'long' }}</h2>
-        <div class="uk-margin-large-top uk-flex uk-flex-space-between uk-flex-wrap" data-uk-margin>
+        <div class="uk-margin-large-top uk-flex uk-flex-middle uk-flex-space-between uk-flex-wrap" data-uk-margin>
             <h3 class="uk-margin-remove" v-if="widget.city">{{ widget.city }}</h3>
-            <h3 class="uk-margin-remove" v-if="status=='done'"><span>{{ temperature }}</span> <img class="uk-margin-small-left" v-attr="src: icon" width="25" height="25" alt="Weather"></h3>
+            <h3 class="uk-flex uk-flex-middle uk-margin-remove" v-if="status=='done'">{{ temperature }} <img class="uk-margin-small-left" v-attr="src: icon" width="25" height="25" alt="Weather"></h3>
         </div>
     </div>
 
