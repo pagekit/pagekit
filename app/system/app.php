0ce1673dc7573f440e57905a7cfb211835170d1a
Steffan <steffan@schiewe.biz>
2015-07-24 15:09:25 +0200

    rename extension.php > index.php rename theme.php > index.php

--- app/system/app.php
+++ app/system/app.php
@@ -3,8 +3,8 @@
 use Pagekit\Module\Loader\ConfigLoader;
 
 $app['module']->addPath([
-    $app['path.extensions'].'/*/extension.php',
-    $app['path.themes'].'/*/theme.php'
+    $app['path.extensions'].'/*/index.php',
+    $app['path.themes'].'/*/index.php'
 ]);
 
 $app['module']->addLoader(new ConfigLoader(require $app['config.file']));
