c075b1a331a8f96cfc8ee5cd6af83c5c9a4b5a5e
nstaeger <mail@nstaeger.de>
2015-07-25 21:18:00 +0200

    add events to debug bar

--- app/modules/debug/app/components/events.vue
+++ app/modules/debug/app/components/events.vue
@@ -2,23 +2,45 @@
 
     <a title="Events"><div class="pf-icon pf-icon-events"></div> Events</a>
 
+    <script id="panel-events" type="text/template">
+
+        <h1>Events</h1>
+
+        <table class="pf-table">
+            <thead>
+                <tr>
+                    <th>Event Name</th>
+                    <th>Listener</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-repeat="events">
+                    <td>{{ name }}</td>
+                    <td><abbr title="{{ listener.details }}">{{ listener.name }}</abbr></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </script>
+
 </template>
 
 <script>
 
-  module.exports = {
+    module.exports = {
 
-    section: {
-        priority: 10
-    },
+        section: {
+            priority: 20,
+            panel: '#panel-events'
+        },
 
-    props: ['data'],
+        props: ['data'],
 
-    ready: function () {
-        this.$data = this.data;
-        this.$parent.add(this);
-    }
+        ready: function () {
+            this.$data = this.data;
+            this.$parent.add(this);
+        }
 
-  };
+    };
 
 </script>
