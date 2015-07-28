7c9a7966eabacfb183a7f017e731aead1a10889b
janschoenherr <jan@yootheme.com>
2015-07-28 10:40:29 +0200

    readded system messages in theme.js

--- app/system/modules/theme/js/theme.js
+++ app/system/modules/theme/js/theme.js
@@ -11,7 +11,7 @@ jQuery(function ($) {
 
             var menu = _(this.menu).sortBy('priority').groupBy('parent').value();
             var item = _.find(menu.root, 'active');
-            var vm   = this;
+            var vm = this;
 
             this.$add('nav', menu.root);
 
@@ -29,12 +29,26 @@ jQuery(function ($) {
                     data[$(this).data('id')] = i;
                 });
 
-                vm.$http.post('admin/adminmenu', {order:data}, function() {
+                vm.$http.post('admin/adminmenu', {order: data}, function () {
                     // message?
                 });
             });
         }
 
+    });
+
+    // show system messages
+    $('.pk-system-messages').children().each(function () {
+
+        var message = $(this), data = message.data();
+
+        // remove success message faster
+        if (data.status && data.status == 'success') {
+            data.timeout = 2000;
+        }
+
+        UIkit.notify(message.html(), data);
+        message.remove();
     });
 
     // offcanvas menu
@@ -44,18 +58,18 @@ jQuery(function ($) {
     // UIkit overrides
     UIkit.notify.message.defaults.timeout = 2000;
 
-    UIkit.modal.alert = function(content, options) {
+    UIkit.modal.alert = function (content, options) {
 
-        options = UIkit.$.extend(true, {modal:false, title: false, labels:UIkit.modal.labels}, options);
+        options = UIkit.$.extend(true, {modal: false, title: false, labels: UIkit.modal.labels}, options);
 
         var modal = UIkit.modal.dialog(([
-            options.title ? '<div class="uk-modal-header"><h2>'+options.title+'</h2></div>':'',
-            '<div class="uk-margin uk-modal-content">'+(options.title ? content : '<h2>'+content+'</h2>')+'</div>',
-            '<div class="uk-modal-footer uk-text-right"><button class="uk-button uk-button-link uk-modal-close">'+options.labels.Ok+'</button></div>'
+            options.title ? '<div class="uk-modal-header"><h2>' + options.title + '</h2></div>' : '',
+            '<div class="uk-margin uk-modal-content">' + (options.title ? content : '<h2>' + content + '</h2>') + '</div>',
+            '<div class="uk-modal-footer uk-text-right"><button class="uk-button uk-button-link uk-modal-close">' + options.labels.Ok + '</button></div>'
         ]).join(""), options);
 
-        modal.on('show.uk.modal', function(){
-            setTimeout(function(){
+        modal.on('show.uk.modal', function () {
+            setTimeout(function () {
                 modal.element.find('button:first').focus();
             }, 50);
         });
@@ -63,24 +77,25 @@ jQuery(function ($) {
         modal.show();
     };
 
-    UIkit.modal.confirm = function(content, onconfirm, options) {
+    UIkit.modal.confirm = function (content, onconfirm, options) {
 
-        onconfirm = UIkit.$.isFunction(onconfirm) ? onconfirm : function(){};
-        options   = UIkit.$.extend(true, {modal:false, title: false, labels:UIkit.modal.labels}, options);
+        onconfirm = UIkit.$.isFunction(onconfirm) ? onconfirm : function () {
+        };
+        options = UIkit.$.extend(true, {modal: false, title: false, labels: UIkit.modal.labels}, options);
 
         var modal = UIkit.modal.dialog(([
-            options.title ? '<div class="uk-modal-header"><h2>'+options.title+'</h2></div>':'',
-            '<div class="uk-margin uk-modal-content">'+(options.title ? content : '<h2>'+content+'</h2>')+'</div>',
-            '<div class="uk-modal-footer uk-text-right"><button class="uk-button uk-button-link uk-modal-close">'+options.labels.Cancel+'</button> <button class="uk-button uk-button-link js-modal-confirm">'+options.labels.Ok+'</button></div>'
+            options.title ? '<div class="uk-modal-header"><h2>' + options.title + '</h2></div>' : '',
+            '<div class="uk-margin uk-modal-content">' + (options.title ? content : '<h2>' + content + '</h2>') + '</div>',
+            '<div class="uk-modal-footer uk-text-right"><button class="uk-button uk-button-link uk-modal-close">' + options.labels.Cancel + '</button> <button class="uk-button uk-button-link js-modal-confirm">' + options.labels.Ok + '</button></div>'
         ]).join(""), options);
 
-        modal.element.find(".js-modal-confirm").on("click", function(){
+        modal.element.find(".js-modal-confirm").on("click", function () {
             onconfirm();
             modal.hide();
         });
 
-        modal.on('show.uk.modal', function(){
-            setTimeout(function(){
+        modal.on('show.uk.modal', function () {
+            setTimeout(function () {
                 modal.element.find('button:first').focus();
             }, 50);
         });
@@ -88,32 +103,33 @@ jQuery(function ($) {
         modal.show();
     };
 
-    UIkit.modal.prompt = function(text, value, onsubmit, options) {
+    UIkit.modal.prompt = function (text, value, onsubmit, options) {
 
-        onsubmit = UIkit.$.isFunction(onsubmit) ? onsubmit : function(value){};
-        options  = UIkit.$.extend(true, {modal:false, title: false, labels:UIkit.modal.labels}, options);
+        onsubmit = UIkit.$.isFunction(onsubmit) ? onsubmit : function (value) {
+        };
+        options = UIkit.$.extend(true, {modal: false, title: false, labels: UIkit.modal.labels}, options);
 
         var modal = UIkit.modal.dialog(([
-            options.title ? '<div class="uk-modal-header"><h2>'+options.title+'</h2></div>':'',
-            text ? '<div class="uk-modal-content uk-form">'+(options.title ? text : '<h2>'+text+'</h2>')+'</div>':'',
-            '<div class="uk-margin-small-top uk-modal-content uk-form"><p><input type="text" class="uk-width-1-1"></p></div>',
-            '<div class="uk-modal-footer uk-text-right"><button class="uk-button uk-button-link uk-modal-close">'+options.labels.Cancel+'</button> <button class="uk-button uk-button-link js-modal-ok">'+options.labels.Ok+'</button></div>'
-        ]).join(""), options),
+                options.title ? '<div class="uk-modal-header"><h2>' + options.title + '</h2></div>' : '',
+                text ? '<div class="uk-modal-content uk-form">' + (options.title ? text : '<h2>' + text + '</h2>') + '</div>' : '',
+                '<div class="uk-margin-small-top uk-modal-content uk-form"><p><input type="text" class="uk-width-1-1"></p></div>',
+                '<div class="uk-modal-footer uk-text-right"><button class="uk-button uk-button-link uk-modal-close">' + options.labels.Cancel + '</button> <button class="uk-button uk-button-link js-modal-ok">' + options.labels.Ok + '</button></div>'
+            ]).join(""), options),
 
-        input = modal.element.find("input[type='text']").val(value || '').on('keyup', function(e){
-            if (e.keyCode == 13) {
-                modal.element.find(".js-modal-ok").trigger('click');
-            }
-        });
+            input = modal.element.find("input[type='text']").val(value || '').on('keyup', function (e) {
+                if (e.keyCode == 13) {
+                    modal.element.find(".js-modal-ok").trigger('click');
+                }
+            });
 
-        modal.element.find(".js-modal-ok").on("click", function(){
-            if (onsubmit(input.val())!==false){
+        modal.element.find(".js-modal-ok").on("click", function () {
+            if (onsubmit(input.val()) !== false) {
                 modal.hide();
             }
         });
 
-        modal.on('show.uk.modal', function(){
-            setTimeout(function(){
+        modal.on('show.uk.modal', function () {
+            setTimeout(function () {
                 input.focus();
             }, 50);
         });
