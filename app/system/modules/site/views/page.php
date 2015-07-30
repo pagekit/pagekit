535c2d1c2b537af166d969bbbac6286714bd2aed
Sascha Dube <sascha.dube@gmail.com>
2015-07-30 13:30:13 +0200

    fixed title_hide for page

--- app/system/modules/site/views/page.php
+++ app/system/modules/site/views/page.php
@@ -1,9 +1,9 @@
 <article class="uk-article">
 
-    <?php if ($page->get('title', true)) : ?>
+    <?php if (!$node->get('title_hide')) : ?>
     <h1 class="uk-article-title"><?= $page->getTitle() ?></h1>
     <?php endif ?>
 
     <?= $page->getContent() ?>
-    
+
 </article>
