6afdf9524512a51d620018eb81e5df479da9cf6c
janschoenherr <jan@yootheme.com>
2015-08-06 11:56:02 +0200

    fix

--- app/system/modules/site/widgets/widget-menu.php
+++ app/system/modules/site/widgets/widget-menu.php
@@ -11,7 +11,7 @@ return [
     'type' => 'widget',
 
     'render' => function ($widget) use ($app) {
-        return App::view()->menu($widget->get('menu'), [
+        return App::view()->menu($widget->get('menu'), 'system/site/widget-menu.php', [
             'start_level' => (int) $widget->get('start_level', 1),
             'depth' => $widget->get('depth'),
             'mode' => $widget->get('mode')
