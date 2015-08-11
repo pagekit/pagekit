22dd2450a42e300c4e5a73a155f67bf91f44d3a3
janschoenherr <jan@yootheme.com>
2015-08-11 17:14:50 +0200

    fixed node moving

--- app/system/modules/site/src/Model/NodeModelTrait.php
+++ app/system/modules/site/src/Model/NodeModelTrait.php
@@ -54,7 +54,7 @@ trait NodeModelTrait
 
         // Update own path
         $path = '/'.$node->slug;
-        if ($node->parent_id && $parent = Node::find($node->parent_id)) {
+        if ($node->parent_id && $parent = Node::find($node->parent_id) and $parent->menu == $node->menu) {
             $path = $parent->path.$path;
         } else {
             // set Parent to 0, if old parent is not found
