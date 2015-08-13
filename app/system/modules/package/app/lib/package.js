616b4294bcbe4249d23d76f5bcad4ca9ed038741
Malte <malte@yootheme.com>
2015-08-13 13:55:45 +0200

    updated package manager

--- app/system/modules/package/app/lib/package.js
+++ app/system/modules/package/app/lib/package.js
@@ -4,28 +4,6 @@ module.exports = {
 
     methods: {
 
-        queryUpdates: function (packages, success) {
-
-            var pkgs = {};
-
-            _.each(packages, function (pkg) {
-                pkgs[pkg.name] = pkg.version;
-            });
-
-            return this.$http.jsonp(this.api.url + '/package/update', {
-                api_key: this.api.key,
-                packages: JSON.stringify(pkgs)
-            }, success);
-        },
-
-        queryPackage: function (pkg, success) {
-            return this.$http.jsonp(this.api.url + '/package/:name', {
-                api_key: this.api.key,
-                name: _.isObject(pkg) ? pkg.name : pkg
-            }, success).error(function () {
-            });
-        },
-
         enablePackage: function (pkg, reload) {
             return this.$http.post('admin/system/package/enable', {name: pkg.name}, function (data) {
                 if (!data.error) {
