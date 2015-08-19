8ce30355e8f11ec778ebd7143d3f3f9d6c8d257a
Malte <malte@yootheme.com>
2015-08-19 16:08:59 +0200

    updated CLI (WIP)

--- app/console/src/Commands/SelfupdateCommand.php
+++ app/console/src/Commands/SelfupdateCommand.php
@@ -19,15 +19,20 @@ class SelfupdateCommand extends Command
 
     const API = 'http://pagekit.com/api';
 
+    protected $cleanFolder = ['app', 'vendor'];
+
+    protected $ignoreFolder = ['vendor/packages'];
+
     /**
      * {@inheritdoc}
      */
     protected $name = 'self-update';
 
+
     /**
      * {@inheritdoc}
      */
-    protected $description = '';
+    protected $description = 'Checks for newer Pagekit versions and installs the latest.';
 
     /**
      * {@inheritdoc}
@@ -44,58 +49,68 @@ class SelfupdateCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->client = new Client;
+        $tmpFile = tmpnam($this->config['path.temp'], 'update_');
 
-        if (!$this->option('url')) {
-            $output->write('Requesting Version...');
-            $versions = $this->getVersions();
+        try {
+            if (!$this->option('url')) {
+                $output->write('Requesting Version...');
+                $versions = $this->getVersions();
+                $output->writeln('<info>done.</info>');
+
+                $output->writeln('');
+                $output->writeln('<comment>Latest Version: ' . $versions['latest']['version'] . '</comment> ');
+                $output->writeln('');
+
+                $helper = $this->getHelper('question');
+                $question = new ConfirmationQuestion('Update to Version ' . $versions['latest']['version'] . '? [y/n]', false);
+
+                if (!$helper->ask($input, $output, $question)) {
+                    return;
+                }
+                $output->writeln('');
+
+                $url = $versions['latest']['url'];
+                $shasum = $versions['latest']['shasum'];
+            } else {
+                $url = $this->option('url');
+                $shasum = $this->option('shasum');
+            }
+
+
+            $output->write('Downloading...');
+            $this->download($url, $shasum, $tmpFile);
             $output->writeln('<info>done.</info>');
 
-            $output->writeln('');
-            $output->writeln('<comment>Latest Version: ' . $versions['latest']['version'] . '</comment> ');
-            $output->writeln('');
+            $output->write('Entering maintenance mode...');
+            $this->setMaintenanceMode(true);
+            $output->writeln('<info>done.</info>');
 
-            $helper = $this->getHelper('question');
-            $question = new ConfirmationQuestion('Update to Version ' . $versions['latest']['version'] . '? [y/n]', false);
+            $output->write('Extracting files...');
+            $this->extract($tmpFile, $this->config['path']);
+            $output->writeln('<info>done.</info>');
 
-            if (!$helper->ask($input, $output, $question)) {
-                return;
-            }
-            $output->writeln('');
+            $output->write('Removing old files...');
+            $this->cleanup($tmpFile, $this->config['path']);
+            unlink($tmpFile);
+            $output->writeln('<info>done.</info>');
 
-            $url = $versions['latest']['url'];
-            $shasum = $versions['latest']['shasum'];
-        } else {
-            $url = $this->option('url');
-            $shasum = $this->option('shasum');
+            $output->write('Migrating database...');
+            $this->database();
+            $output->writeln('<info>done.</info>');
+
+            $output->write('Deactivating maintenance mode...');
+            $this->setMaintenanceMode(false);
+            $output->writeln('<info>done.</info>');
+        } catch (\Exception $e) {
+            unlink($tmpFile);
+            throw $e;
         }
 
-        $tmpFile = $this->config['path.temp'] . '/' . sha1(uniqid());
-
-        $output->write('Downloading...');
-        $this->download($url, $shasum, $tmpFile);
-        $output->writeln('<info>done.</info>');
-
-        $output->write('Entering maintenance mode...');
-        $this->setMaintenanceMode(true);
-        $output->writeln('<info>done.</info>');
-
-        $output->write('Extracting files...');
-        $this->extract($tmpFile, $this->config['path']);
-        $output->writeln('<info>done.</info>');
-
-        $output->write('Migrating database...');
-        $this->database();
-        $output->writeln('<info>done.</info>');
-
-        //TODO: Clear cache
-
-        $output->write('Deactivating maintenance mode...');
-        $this->setMaintenanceMode(false);
-        $output->writeln('<info>done.</info>');
     }
 
     /**
      * @return mixed
+     * @throws \Exception
      */
     protected function getVersions()
     {
@@ -103,7 +118,7 @@ class SelfupdateCommand extends Command
             $res = $this->client->get(self::API . '/update');
         } catch (\Exception $e) {
             if ($e instanceof TransferException) {
-                throw new \RuntimeException(__('Could not obtain latest Version.'));
+                throw new \RuntimeException('Could not obtain latest Version.');
             }
             throw $e;
         }
@@ -121,52 +136,47 @@ class SelfupdateCommand extends Command
     {
         try {
             if (!$url) {
-                throw new \RuntimeException(__('Package url is missing.'));
+                throw new \RuntimeException('Package url is missing.');
             }
 
             $data = $this->client->get($url)->getBody();
 
             if (sha1($data) !== $shasum) {
-                throw new \RuntimeException(__('Package checksum verification failed.'));
+                throw new \RuntimeException('Package checksum verification failed.');
             }
 
             if (!file_put_contents($file, $data)) {
-                throw new \RuntimeException(__('Path is not writable.'));
+                throw new \RuntimeException('Path is not writable.');
             }
 
         } catch (\Exception $e) {
-            unlink($file);
-
             if ($e instanceof TransferException) {
                 if ($e instanceof BadResponseException) {
-                    throw new \RuntimeException(__('Invalid API key.'));
+                    throw new \RuntimeException('Invalid API key.');
                 }
-                throw new \RuntimeException(__('Package download failed.'));
+                throw new \RuntimeException('Package download failed.');
             }
             throw $e;
         }
     }
 
     /**
-     * @param $file
+     * @param $tmpFile
      * @param $path
      */
-    protected function extract($file, $path)
+    protected function extract($tmpFile, $path)
     {
         $zip = new \ZipArchive;
-        if ($zip->open($file) === true) {
+        if ($zip->open($tmpFile) === true) {
 
             $zip->deleteName(".htaccess");
 
             $zip->extractTo($path);
             $zip->close();
         } else {
-            throw new \RuntimeException(__('Package extraction failed.'));
+            throw new \RuntimeException('Package extraction failed.');
         }
-
-        unlink($file);
     }
-
 
     /**
      * Migrating the database.
@@ -184,6 +194,46 @@ class SelfupdateCommand extends Command
         }
     }
 
+    /**
+     * @param $tmpFile
+     * @param $base
+     */
+    protected function cleanup($tmpFile, $base)
+    {
+        $zip = new \ZipArchive;
+        if ($zip->open($tmpFile) === true) {
+
+            foreach ($this->cleanFolder as $folder) {
+                $this->doCleanup($zip, $base, $folder);
+            }
+
+            $zip->close();
+        } else {
+            throw new \RuntimeException('Package extraction failed.');
+        }
+    }
+
+    /**
+     * @param $zip
+     * @param $base
+     * @param $path
+     */
+    protected function doCleanup($zip, $base, $path)
+    {
+        foreach (array_diff(scandir($this->config['path'] . '/' . $path), array('..', '.')) as $file) {
+            $file = ($path ? $path . '/' : '') . $file;
+            if (is_dir($base . '/' . $file)) {
+                if (in_array($file, $this->ignoreFolder)) {
+                    continue;
+                }
+
+                $this->doCleanup($zip, $base, $file);
+                @rmdir($base . '/' . $file);
+            } else if (!$zip->statName($file)) {
+                unlink($base . '/' . $file);
+            }
+        }
+    }
 
     /**
      * Toggles maintenance mode without booting Pagekit application.
@@ -204,10 +254,10 @@ class SelfupdateCommand extends Command
             $config['system/site']['maintenance']['enabled'] = true;
         } else {
             unset ($config['system/site']['maintenance']['enabled']);
-            if (!count($config['system/site']['maintenance'])) {
+            if (isset($config['system/site']['maintenance']) && !count($config['system/site']['maintenance'])) {
                 unset($config['system/site']['maintenance']);
             }
-            if (!count($config['system/site'])) {
+            if (isset($config['system/site']) && !count($config['system/site'])) {
                 unset($config['system/site']);
             }
         }
