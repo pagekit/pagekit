61866730acf9ea85e35d248013429e2d0c057948
Malte <malte@yootheme.com>
2015-08-19 18:25:04 +0200

    updated CLI (WIP)

--- app/console/src/Application.php
+++ app/console/src/Application.php
@@ -61,24 +61,7 @@ class Application extends BaseApplication
     public function getPagekit()
     {
         if (!$this->pagekit) {
-            $loader = require __DIR__ . '/../../autoload.php';
-
-            $app = new App($this->config['values']);
-            $app['autoloader'] = $loader;
-
-            $app['module']->addPath([
-                __DIR__ . '/../../modules/*/index.php',
-                __DIR__ . '/../../system/index.php',
-            ]);
-
-            $app['module']->addLoader(new AutoLoader($loader));
-            $app['module']->addLoader(new ConfigLoader($this->config));
-            $app['module']->addLoader(new ConfigLoader(require $app['config.file']));
-
-            $app['module']->addPath(__DIR__ . '/../console.php');
-            $app['module']->load('console');
-
-            $this->pagekit = $app;
+            $this->pagekit = require __DIR__ . '/../autoload.php';
         }
 
         return $this->pagekit;
