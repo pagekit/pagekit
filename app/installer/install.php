adeaef2dc18422a89e75773d576fe133717b0712
Florian Letsch <florianletsch@gmail.com>
2015-08-26 17:22:51 +0200

    added blog to sample data. not the prettiest right now.

--- app/installer/install.php
+++ app/installer/install.php
@@ -6,13 +6,20 @@ use Pagekit\Application as App;
  * Post install script. Insert sample data. *
  ********************************************/
 
+// Enable blog. TODO: find a prettier way
+
+$app = App::getInstance();
+$blogpath = $app['path.extensions'].'/blog';
+if ($version = $app['migrator']->create($blogpath.'/migrations')->run()) {
+    $app['config']('blog')->set('version', $version);
+}
+
 App::config()->set('system', [
     'site' => [
         'theme' => 'one'
     ],
     'extensions' => ['blog']
 ]);
-
 
 App::config()->set('system/site', [
     'menus' =>
@@ -21,7 +28,7 @@ App::config()->set('system/site', [
             'label' => 'Main'
         ]
     ],
-    // 'frontpage' => true  ???
+    'frontpage' => 1
 ]);
 
 App::config()->set('system/dashboard', [
@@ -72,28 +79,28 @@ App::config()->set('one', [
     ]
 ]);
 
-// $sql = <<<EOT
-// -- blog sample data
-// INSERT INTO `@blog_post` (`id`, `user_id`, `slug`, `title`, `status`, `date`, `modified`, `content`, `excerpt`, `comment_status`, `comment_count`, `data`, `roles`)
-// VALUES
-//     (1,1,'hello-pagekit','Hello Pagekit',2,'2015-08-26 11:52:04','2015-08-26 11:52:43','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.','',1,0,'{\"title\":null,\"markdown\":true}',NULL);
+$sql = <<<EOT
+-- blog sample data
+INSERT INTO `@blog_post` (`id`, `user_id`, `slug`, `title`, `status`, `date`, `modified`, `content`, `excerpt`, `comment_status`, `comment_count`, `data`, `roles`)
+VALUES
+    (1,1,'hello-pagekit','Hello Pagekit',2,'2015-08-26 11:52:04','2015-08-26 11:52:43','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.','',1,0,'{\"title\":null,\"markdown\":true}',NULL);
 
-// -- pages sample data
-// INSERT INTO `@system_page` (`id`, `title`, `content`, `data`)
-// VALUES
-//     (1,'Home','<p>\n   Hello and welcome to the Pagekit demo content. \n</p>\n\n<p>\n    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    \n</p>\n','{\"title\":true}');
+-- pages sample data
+INSERT INTO `@system_page` (`id`, `title`, `content`, `data`)
+VALUES
+    (1,'Home','<p>\n   Hello and welcome to the Pagekit demo content. \n</p>\n\n<p>\n    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    \n</p>\n','{\"title\":true}');
 
-// -- system nodes
-// INSERT INTO `@system_node` (`id`, `parent_id`, `priority`, `status`, `title`, `slug`, `path`, `link`, `type`, `menu`, `roles`, `data`)
-// VALUES
-//     (1,0,1,1,'Home','home','/home','@page/1','page','main',NULL,'{\"defaults\":{\"id\":1}}'),
-//     (2,0,2,1,'Blog','blog','/blog','@blog','blog','main',NULL,'[]');
-// EOT;
+-- system nodes
+INSERT INTO `@system_node` (`id`, `parent_id`, `priority`, `status`, `title`, `slug`, `path`, `link`, `type`, `menu`, `roles`, `data`)
+VALUES
+    (1,0,1,1,'Home','home','/home','@page/1','page','main',NULL,'{\"defaults\":{\"id\":1}}'),
+    (2,0,2,1,'Blog','blog','/blog','@blog','blog','main',NULL,'[]');
+EOT;
 
-// foreach (explode(';', $sql) as $query) {
-//     if ($query = trim($query)) {
-//         App::db()->executeUpdate($query);
-//     }
-// }
+foreach (explode(';', $sql) as $query) {
+    if ($query = trim($query)) {
+        App::db()->executeUpdate($query);
+    }
+}
 
 
