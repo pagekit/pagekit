14bfef4fd4f1a453b82e6c64473b9be71f765458
Florian Letsch <florianletsch@gmail.com>
2015-09-01 10:33:24 +0200

    fixed blank view

--- app/system/modules/theme/views/blank.php
+++ app/system/modules/theme/views/blank.php
@@ -1,4 +1,4 @@
-<html lang="<?= str_replace('_', '-', $app['translator']->getLocaleTag()) ?>">
+<html lang="<?= str_replace('_', '-', $app['translator']->getLocale()) ?>">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
