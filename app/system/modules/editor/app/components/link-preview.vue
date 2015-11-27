6150dddc35d9f18af04d27300de9f98675e79b22
Malte <malte@yootheme.com>
2015-11-27 16:39:47 +0100

    fix link preview

--- app/system/modules/editor/app/components/link-preview.vue
+++ app/system/modules/editor/app/components/link-preview.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <a :class="[link.class]" :href="link.link" @click.prevent="$parent.openModal(link)">{{{ link.txt ? link.txt : 'Select Link' | trans }}} <span class="pk-icon-link pk-icon-hover"></span></a>
+    <a :class="link.class" :href="link.link" @click.prevent="$parent.openModal(link)">{{{ link.txt ? link.txt : 'Select Link' | trans }}} <span class="pk-icon-link pk-icon-hover"></span></a>
 
 </template>
 
