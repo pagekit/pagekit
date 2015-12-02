bfd7c05b02db0950eae934d23eb20dcf7adda23d
Florian <florian@yootheme.com>
2015-12-02 14:19:15 +0100

    use partial instead of component

--- app/system/modules/site/app/components/input-tree.vue
+++ app/system/modules/site/app/components/input-tree.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="uk-list uk-margin-top-remove" v-for="menu in menus" v-if="menu.getNodes().length">
         <li class="pk-list-header">{{ menu.label }}</li>
-        <node v-for="node in menu.getNodes()" :nodes.sync="nodes" :node="node" :menu="menu"></node>
+        <partial name="node-partial" v-for="node in menu.getNodes()"></partial>
     </ul>
 </template>
 
@@ -61,25 +61,16 @@
             }
         },
 
-        components: {
+        partials: {
 
-            node: {
+            'node-partial': '<li>'+
+                '<label>' +
+                    '<input type="checkbox" :value="node.id" v-model="nodes" number>' +
+                    ' {{ node.title }}' +
+                '</label>' +
+                '<partial name="node-partial" v-for="node in menu.getNodes(node)"></partial>' +
+            '<li>'
 
-                props: ['nodes', 'node', 'menu'],
-
-                name: 'node',
-
-                template: '<li>'+
-                    '<label>' +
-                        '<input type="checkbox" :value="node.id" v-model="nodes" number>' +
-                        ' {{ node.title }}' +
-                    '</label>' +
-                    '<ul class="uk-list" v-if="menu.getNodes(node)">' +
-                        '<node v-for="node in menu.getNodes(node)" :nodes.sync="nodes" :node="node" :menu="menu"></node>' +
-                    '</ul>'+
-                '<li>'
-
-            }
         }
     }
 
