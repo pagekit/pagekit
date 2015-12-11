b770b30efb46e1a1d3a74fdfee85ce4910544f2a
Malte <malte@yootheme.com>
2015-12-11 19:31:08 +0100

    fix uncaught promise issue, drop extend promise functions

--- app/system/app/lib/asset.js
+++ app/system/app/lib/asset.js
@@ -9,7 +9,7 @@ module.exports = function (Vue) {
      */
     function Asset(assets, success, error) {
 
-        var self = this, promises = [], $url = (this.$url || Vue.url), _assets = [], promise;
+        var promises = [], $url = (this.$url || Vue.url), _assets = [];
 
         Object.keys(assets).forEach(function (type) {
 
@@ -34,46 +34,8 @@ module.exports = function (Vue) {
 
         });
 
-        promise = Promise.all(promises);
+        return Promise.all(promises).then(success, error);
 
-        promise.success = function (fn) {
-
-            promise.then(function (response) {
-                fn.call(self, response);
-            });
-
-            return promise;
-        };
-
-        promise.error = function (fn) {
-
-            promise.then(undefined, function (response) {
-                fn.call(self, response);
-            });
-
-            return promise;
-        };
-
-        promise.always = function (fn) {
-
-            var cb = function (response) {
-                fn.call(self, response);
-            };
-
-            promise.then(cb, cb);
-
-            return promise;
-        };
-
-        if (success) {
-            promise.success(success);
-        }
-
-        if (error) {
-            promise.error(error);
-        }
-
-        return promise;
     }
 
     _.extend(Asset, {
