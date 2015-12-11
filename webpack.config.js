9323abf73b42b548791b6d91559a90b554934bdf
janschoenherr <jan@yootheme.com>
2015-12-11 14:07:41 +0100

    updated package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var exports = [];
 glob.sync('{app/modules/**,app/installer/**,app/system/**,packages/**}/webpack.config.js').forEach(function (file) {
     var dir = path.join(__dirname, path.dirname(file));
     exports = exports.concat(require('./' + file).map(function (config) {
-        return _.merge({ context: dir, output: { path: dir } }, config);
+        return _.merge({context: dir, output: {path: dir}}, config);
     }));
 });
 
