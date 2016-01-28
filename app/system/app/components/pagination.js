6ae225d3c7185d98a143e945612e50e41cb2f83e
Malte <malte@yootheme.com>
2016-01-28 10:48:10 +0100

    pagination cache (WIP)

--- app/system/app/components/pagination.js
+++ app/system/app/components/pagination.js
@@ -14,13 +14,33 @@ module.exports = {
         }
     },
 
-    ready: function() {
+    created: function () {
+
+        this.key = this.$parent.$options.name + '.pagination';
+
+        if (this.page === undefined && this.$session.get(this.key)) {
+            this.$set('page', this.$session.get(this.key));
+            history.replaceState({page: this.$session.get(this.key)}, '', '?page=' + this.$session.get(this.key));
+        }
+
+        var vm = this;
+        window.onpopstate = function (event) {
+
+            if (event.state) {
+                vm.page = event.state.page;
+            } else {
+                vm.page = 0;
+            }
+
+        };
+    },
+
+    ready: function () {
 
         var vm = this;
 
-        this.pagination = UIkit.pagination(this.$el, { pages: this.pages, currentPage: this.page });
-
-        this.pagination.on('select.uk.pagination', function(e, page) {
+        this.pagination = UIkit.pagination(this.$el, {pages: this.pages, currentPage: this.page});
+        this.pagination.on('select.uk.pagination', function (e, page) {
             vm.$set('page', page);
         });
 
@@ -28,11 +48,13 @@ module.exports = {
 
     watch: {
 
-        page: function(page) {
+        page: function (page) {
             this.pagination.selectPage(page);
+            history.pushState({page: page}, '', '?page=' + page);
+            this.$session.set(this.key, page);
         },
 
-        pages: function(pages) {
+        pages: function (pages) {
             this.pagination.render(pages);
         }
 
