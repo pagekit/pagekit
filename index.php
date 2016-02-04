b31ed9715013f6293760b025a967453c2b7628eb
Malte <malte@yootheme.com>
2016-02-04 14:32:54 +0100

    update PHP version check at index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (version_compare($ver = PHP_VERSION, $req = '5.4.0', '<')) {
+if (version_compare($ver = PHP_VERSION, $req = '5.5.9', '<')) {
     exit(sprintf('You are running PHP %s, but Pagekit needs at least <strong>PHP %s</strong> to run.', $ver, $req));
 }
 
