49b68d2676561f60ebae9ed6b5942d7dd14e1ae3
Florian <florian@yootheme.com>
2016-02-29 14:18:29 +0100

    Fixed single quotes issue in URL rewrites. re #511

--- app/system/modules/view/src/Event/ResponseListener.php
+++ app/system/modules/view/src/Event/ResponseListener.php
@@ -10,7 +10,7 @@ class ResponseListener implements EventSubscriberInterface
     const REGEX_URL = '/
                         \s                              # match a space
                         (?<attr>href|src|poster)=       # match the attribute
-                        ([\"\'])                        # start with a single or double quote
+                        (?<quote>[\"\'])                # start with a single or double quote
                         (?!\/|\#|[a-z0-9\-\.]+\:)       # make sure it is a relative path
                         (?<url>[^\"\'>]+)               # match the actual src value
                         \2                              # match the previous quote
@@ -26,7 +26,7 @@ class ResponseListener implements EventSubscriberInterface
         }
 
         $response->setContent(preg_replace_callback(self::REGEX_URL, function ($matches) {
-            return sprintf(' %s="%s"', $matches['attr'], App::url($matches['url']));
+            return sprintf(' %s=%s%s%s', $matches['attr'], $matches['quote'], App::url($matches['url']), $matches['quote']);
         }, $content));
     }
 
