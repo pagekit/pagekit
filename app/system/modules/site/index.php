e846e49992c4ee8db2efeca8a1200772433a30a1
Malte <malte@yootheme.com>
2016-04-14 13:51:46 +0200

    ensure that OpenGraph image is absolute

--- app/system/modules/site/index.php
+++ app/system/modules/site/index.php
@@ -185,7 +185,13 @@ return [
                 ]);
 
                 if ($app['node']->get('meta')) {
-                    $meta($app['node']->get('meta'));
+                    $config = $app['node']->get('meta');
+
+                    if ($config['og:image']) {
+                        $config['og:image'] = $app['url']->getStatic($config['og:image'], [], 0);
+                    }
+
+                    $meta($config);
                 }
 
             }, 50);
