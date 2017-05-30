5f01f78f8d397ea9ea1d69a93c5db8370e245630
Malte <malte@yootheme.com>
2017-05-30 14:25:03 +0200

    fixes #826

--- app/system/modules/settings/src/Controller/SettingsController.php
+++ app/system/modules/settings/src/Controller/SettingsController.php
@@ -50,7 +50,7 @@ class SettingsController
      */
     public function configAction($name, $config = [])
     {
-        App::config($name)->merge($config, true);
+        App::config()->set($name, $config);
 
         return ['message' => 'success'];
     }
